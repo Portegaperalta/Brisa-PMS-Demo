626533e290b7c21a396a0a9fdac87a877d3400ed
pablo <ortegaperaltap@gmail.com>
2026-03-26 13:30:17 -0400

    Create domain entity Company with attributes and constructor

---
+++ BrisaPMS.Domain/Entities/Company.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace BrisaPMS.Domain.Entities;
+
+public class Company
+{
+    public Guid Id { get; private init; } =  Guid.NewGuid();
+    public string LegalName {get; private set;}
+    public string CommercialName {get; private set;}
+    public string Rnc {get; private init;}
+    public string BusinessEmail { get; private set; }
+    public string BusinessPhone { get; private set; }
+    public string LogoUrl  { get; private set; }
+    public string Address1 { get; private set; }
+    public string Address2 { get; private set; }
+    public string City { get; private set; }
+    public string Province { get; private set; }
+    public string ZipCode {get; private set;}
+    public DateTime? UpdatedAt { get; private set; }
+
+    public Company(string legalName, 
+        string commercialName, 
+        string rnc,
+        string businessEmail, 
+        string businessPhone, 
+        string logoUrl, 
+        string address1,
+        string address2, 
+        string city, 
+        string province, 
+        string zipCode)
+    {
+        if (string.IsNullOrEmpty(legalName) is true)
+            throw new ArgumentException("Legal name cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(commercialName) is true)
+            throw new ArgumentException("Commercial name cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(rnc) is true)
+            throw new ArgumentException("Rnc cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(businessEmail) is true)
+            throw new ArgumentException("Business email cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(businessPhone) is true)
+            throw new ArgumentException("Business phone number cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(address1) is true)
+            throw new ArgumentException("Address 1 cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(city) is true)
+            throw new ArgumentException("City cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(province) is true)
+            throw new ArgumentException("Province cannot be empty.");
+        
+        if (string.IsNullOrWhiteSpace(zipCode) is true)
+            throw new ArgumentException("Zip code cannot be empty.");
+        
+        LegalName = legalName;
+        CommercialName = commercialName;
+        Rnc = rnc;
+        BusinessEmail = businessEmail;
+        BusinessPhone = businessPhone;
+        LogoUrl = logoUrl;
+        Address1 = address1;
+        Address2 = address2;
+        City = city;
+        Province = province;
+        ZipCode = zipCode;
+        UpdatedAt = null;
+    }
+}
