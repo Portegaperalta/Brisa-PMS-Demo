7e6333ebec5777fc1fd758c8aa8125a7023e8e5f
Pablo <ortegaperaltap@gmail.com>
2026-03-25 20:47:05 -0400

    Create custom ExpiredLockoutEndDateException

---
+++ BrisaPMS.Domain/Exceptions/ExpiredDateException.cs
@@ -0,0 +1,6 @@
+﻿namespace BrisaPMS.Domain.Exceptions;
+
+class ExpiredLockoutEndDateException : Exception
+{
+    public ExpiredLockoutEndDateException() : base("Lockout expiry date cannot be older than current date") { }
+}
