e6a63b0777abc9b23ff432c17703166d3818a695
Pablo <ortegaperaltap@gmail.com>
2026-03-27 09:17:36 -0400

    Create custom InvalidPasswordException

---
+++ BrisaPMS.Domain/Exceptions/InvalidValueExceptions/InvalidPasswordException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BrisaPMS.Domain.Exceptions.InvalidValueExceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException() : base("Password must be at least 8 characters and include an uppercase letter and a special character.") { }
+    }
+}
