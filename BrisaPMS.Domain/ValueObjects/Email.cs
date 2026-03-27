1cb16b536c08acfa4a78dbd3e246e6cdcfb9081d
Pablo <ortegaperaltap@gmail.com>
2026-03-27 17:10:58 -0400

    Create unit tests for value object Email and fix its format and character length checks

--- BrisaPMS.Domain/ValueObjects/Email.cs
+++ BrisaPMS.Domain/ValueObjects/Email.cs
@@ -13,10 +13,10 @@ namespace BrisaPMS.Domain.ValueObjects
             if (string.IsNullOrWhiteSpace(email) is true)
                 throw new EmptyRequiredFieldException("Email");
 
-            if (email.Contains('@') != true && email.Contains('.') != true)
+            if (email.Contains('@') != true || email.Contains('.') != true)
                 throw new InvalidFieldException("Email", "The email must be a valid email address");
 
-            if (email.Length > 254)
+            if (email.Length > MaxCharacterLimit)
                 throw new MaxCharacterLimitException(MaxCharacterLimit, "Email");
 
             Value = email;
