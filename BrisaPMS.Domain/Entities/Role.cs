3d32a90bf655acc198be3c126324eb9707b5d748
pablo <ortegaperaltap@gmail.com>
2026-03-29 16:27:54 -0400

    Add class constructor

--- BrisaPMS.Domain/Entities/Role.cs
+++ BrisaPMS.Domain/Entities/Role.cs
@@ -6,4 +6,10 @@ public class Role
 {
     public Guid Id { get; private set; }
     public RoleType Type { get; private set; }
+
+    public Role(RoleType type)
+    {
+        Id = Guid.CreateVersion7();
+        Type = type;
+    }
 }
