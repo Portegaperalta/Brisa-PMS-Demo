bc5adf1ab7434683aa06ce4686e565e1fa7e9ffd
pablo <ortegaperaltap@gmail.com>
2026-03-30 19:00:56 -0400

    Create Room entity

---
+++ BrisaPMS.Domain/Entities/Room.cs
@@ -0,0 +1,112 @@
+using BrisaPMS.Domain.Enums;
+using BrisaPMS.Domain.Exceptions;
+
+namespace BrisaPMS.Domain.Entities;
+
+public class Room
+{
+    public Guid Id { get; init; }
+    public Guid HotelId { get; init ; }
+    public RoomType RoomType { get; private set ; }
+    public string Number { get; private set ; }
+    public int Floor { get; init ; }
+    public int TotalBeds { get; private set ; }
+    public AvailabilityStatus AvailabilityStatus { get; private set ; }
+    public HygieneStatus HygieneStatus { get; private set ; }
+    public DateTime? LastCleanedAt { get; private set ; }
+    public Guid? LastCleanedBy { get; private set ; }
+    public bool NeedsRestocking { get; private set ; }
+    
+    public Room(Guid hotelId,
+        RoomType roomType,
+        string number,
+        int floor,
+        int totalBeds,
+        AvailabilityStatus availabilityStatus,
+        HygieneStatus hygieneStatus)
+    {
+        if (hotelId == Guid.Empty)
+            throw new EmptyRequiredFieldException("HotelId");
+        
+        if (Enum.IsDefined<RoomType>(roomType) is false)
+            throw new BusinessRuleException("Invalid room type");
+        
+        if(string.IsNullOrWhiteSpace(number))
+            throw new EmptyRequiredFieldException("Room number");
+        
+        if (totalBeds <= 0) 
+            throw new BusinessRuleException("Room must have at least one bed");
+        
+        if(Enum.IsDefined<AvailabilityStatus>(availabilityStatus) is false)
+            throw new BusinessRuleException("Invalid availability status");
+        
+        if (Enum.IsDefined<HygieneStatus>(hygieneStatus) is false)
+            throw new BusinessRuleException("Invalid hygiene status");
+
+        Id = Guid.CreateVersion7();
+        HotelId = hotelId;
+        RoomType = roomType;
+        Number = number;
+        Floor = floor;
+        TotalBeds = totalBeds;
+        AvailabilityStatus = availabilityStatus;
+        HygieneStatus = hygieneStatus;
+        LastCleanedAt = null;
+        LastCleanedBy = null;
+        NeedsRestocking = false;
+    }
+
+    public void ChangeRoomType(RoomType newRoomType)
+    {
+        if (Enum.IsDefined<RoomType>(newRoomType) is false)
+            throw new BusinessRuleException("Invalid room type");
+        
+        RoomType = newRoomType;
+    }
+
+    public void ChangeNumber(string newRoomNumber)
+    {
+        if (string.IsNullOrWhiteSpace(newRoomNumber))
+            throw new EmptyRequiredFieldException("Room number");
+
+        Number = newRoomNumber;
+    }
+
+    public void ChangeTotalBeds(int newTotalBeds)
+    {
+        if (newTotalBeds <= 0)
+            throw new BusinessRuleException("Room must have at least one bed");
+        
+        TotalBeds = newTotalBeds;
+    }
+
+    public void ChangeAvailabilityStatus(AvailabilityStatus newAvailabilityStatus)
+    {
+        if(Enum.IsDefined<AvailabilityStatus>(newAvailabilityStatus) is false)
+            throw new BusinessRuleException("Invalid availability status");
+        
+        AvailabilityStatus = newAvailabilityStatus;
+    }
+
+    public void ChangeHygieneStatus(HygieneStatus newHygieneStatus)
+    {
+        if (Enum.IsDefined<HygieneStatus>(newHygieneStatus) is false)
+            throw new BusinessRuleException("Invalid hygiene status");
+        
+        HygieneStatus = newHygieneStatus;
+    }
+    
+    public void UpdateLastCleanedAt() => LastCleanedAt = DateTime.UtcNow;
+
+    public void UpdateLastCleanedBy(Guid userId)
+    {
+        if (userId == Guid.Empty)
+            throw new EmptyRequiredFieldException("User id");
+        
+        LastCleanedBy = userId;
+    }
+
+    public void SetAsPendingRestocking() => NeedsRestocking = true;
+    
+    public void SetAsRestocked() => NeedsRestocking = false;
+}
