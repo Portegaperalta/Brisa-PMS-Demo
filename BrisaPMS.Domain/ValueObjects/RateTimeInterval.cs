780b9fa39b230198fb77d92a7917e206464e6f3a
Pablo <ortegaperaltap@gmail.com>
2026-03-31 07:31:31 -0400

    Create value object RateTimeInterval

---
+++ BrisaPMS.Domain/ValueObjects/RateTimeInterval.cs
@@ -0,0 +1,21 @@
+﻿using BrisaPMS.Domain.Exceptions;
+
+namespace BrisaPMS.Domain.ValueObjects;
+
+public class RateTimeInterval
+{
+    public DateTime ValidFrom { get; }
+    public DateTime ValidTo { get; }
+
+    public RateTimeInterval(DateTime validFrom, DateTime validTo)
+    {
+        if (validFrom > validTo)
+            throw new BusinessRuleException("Rate start time must be less than or equal to end time");
+        
+        if (validTo < validFrom)
+            throw new BusinessRuleException("Rate end time must be greater than start time");
+        
+        ValidFrom = validFrom;
+        ValidTo = validTo;
+    }
+}
