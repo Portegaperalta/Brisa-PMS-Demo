6b52a1a2d3b64e906cdcdf9f62c7f3d87ea907e5
pablo <ortegaperaltap@gmail.com>
2026-04-02 18:46:50 -0400

    Create enum "InvoiceType"

---
+++ BrisaPMS.Domain/Enums/InvoiceType.cs
@@ -0,0 +1,16 @@
+namespace BrisaPMS.Domain.Enums;
+
+public enum InvoiceType
+{
+    Guest = 1,
+    Group = 2,
+    CityLedger = 3,
+    RestaurantCheck = 4,
+    BanquetInvoice = 5,
+    FacturaConsumo = 6,
+    FacturaCredito = 7,
+    NotaDeCredito = 8,
+    NotaDeDebito = 9,
+    RegimeEspecial = 10,
+    Purchase = 11
+}
