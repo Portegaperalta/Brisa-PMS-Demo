dfffd35283619e178f7ca6336ab14a072e4b7e18
Pablo <ortegaperaltap@gmail.com>
2026-04-02 07:41:46 -0400

    Create entity "Discount" an add atrributes

---
+++ BrisaPMS.Domain/Entities/Discount.cs
@@ -0,0 +1,14 @@
+﻿using BrisaPMS.Domain.ValueObjects;
+
+namespace BrisaPMS.Domain.Entities;
+
+public class Discount
+{
+    public Guid Id { get; }
+    public Guid HotelId { get; init; }
+    public string Name { get; private set; }
+    public string Type { get; private set; }
+    public decimal Value { get;  private set; }
+    public DiscountTimeInterval TimeInterval { get; private set; }
+    public bool IsActive { get; private set; }
+}
