3ff832928efb8ebcc7d5108cc5cd1830c2a1b24a
pablo <ortegaperaltap@gmail.com>
2026-04-04 14:47:48 -0400

    Modify GetById return type to be nullable

--- BrisaPMS.Application/Contracts/Repositories/IRepository.cs
+++ BrisaPMS.Application/Contracts/Repositories/IRepository.cs
@@ -2,8 +2,8 @@ namespace BrisaPMS.Application.Contracts.Repositories;
 
 public interface IRepository<T> where T : class
 {
-    Task<T> GetById(Guid id);
-    Task<IEnumerable<T> > GetAll();
+    Task<T?> GetById(Guid id);
+    Task<IEnumerable<T>> GetAll();
     Task Create(T entity);
     Task Update(T entity);
     Task Delete(T entity);
