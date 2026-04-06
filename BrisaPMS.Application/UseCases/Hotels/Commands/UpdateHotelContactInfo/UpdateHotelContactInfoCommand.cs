c421ee813b6ed2a622123c9c30f31efbb224f5da
pablo <ortegaperaltap@gmail.com>
2026-04-05 22:05:05 -0400

    Replace email and phone attributes type to primitive string types

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelContactInfo/UpdateHotelContactInfoCommand.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelContactInfo/UpdateHotelContactInfoCommand.cs
@@ -5,6 +5,6 @@ namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelContactInfo;
 public class UpdateHotelContactInfoCommand
 {
     public required Guid Id { get; set; }
-    public required Email BusinessEmail { get; set; }
-    public required PhoneNumber BusinessPhoneNumber { get; set; }
+    public required string BusinessEmail { get; set; }
+    public required string BusinessPhoneNumber { get; set; }
 }
