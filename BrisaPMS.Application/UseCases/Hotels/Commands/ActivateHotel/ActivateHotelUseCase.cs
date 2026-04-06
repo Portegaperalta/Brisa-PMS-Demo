e5bb52ecb1cb8c4a0511912b6acccd1fe0a4ccce
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:45:51 -0400

    Inject validator dependency and throw validation errors if validator fails

--- BrisaPMS.Application/UseCases/Hotels/Commands/ActivateHotel/ActivateHotelUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/ActivateHotel/ActivateHotelUseCase.cs
@@ -1,5 +1,7 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.ActivateHotel;
 
@@ -7,15 +9,23 @@ public class ActivateHotelUseCase
 {
     private readonly IHotelsRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<ActivateHotelCommand> _validator;
     
-    public ActivateHotelUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork)
+    public ActivateHotelUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork,
+        IValidator<ActivateHotelCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
 
     public async Task Handle(ActivateHotelCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
         var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
--- BrisaPMS.Application/UseCases/Hotels/Commands/DeactivateHotel/DeactivateHotelUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/DeactivateHotel/DeactivateHotelUseCase.cs
@@ -1,5 +1,7 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.DeactivateHotel;
 
@@ -7,15 +9,23 @@ public class DeactivateHotelUseCase
 {
     private readonly IHotelsRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<DeactivateHotelCommand> _validator;
 
-    public DeactivateHotelUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork)
+    public DeactivateHotelUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork,
+        IValidator<DeactivateHotelCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
 
     public async Task Handle(DeactivateHotelCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
         var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
