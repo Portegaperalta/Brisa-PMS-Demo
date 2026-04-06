ae0350a0164799a63a239e25f4d4620cf1d89cf4
pablo <ortegaperaltap@gmail.com>
2026-04-05 21:24:19 -0400

    1. Inject Validator dependency and throw custom ValidationException if validations for CreateHotelCommand fail.
    2. Transform primitive types in command to Value Objects required to create new Hotel entity

--- BrisaPMS.Application/UseCases/Hotels/Commands/CreateHotel/CreateHotelUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/CreateHotel/CreateHotelUseCase.cs
@@ -1,6 +1,10 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Domain.Billing;
 using BrisaPMS.Domain.Hotels;
+using BrisaPMS.Domain.Shared.ValueObjects;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.CreateHotel;
 
@@ -8,32 +12,48 @@ public class CreateHotelUseCase
 {
     private readonly IHotelsRepository _repository;
     private readonly IUnitOfWork  _unitOfWork;
+    private readonly IValidator<CreateHotelCommand> _validator;
     
-    public CreateHotelUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork)
+    public CreateHotelUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork, 
+        IValidator<CreateHotelCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
     
     public async Task<Guid> Handle(CreateHotelCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
+        var businessEmail = new Email(command.BusinessEmail);
+        var businessPhoneNumber = new PhoneNumber(command.BusinessPhoneNumber);
+        var logoUrl = new Url(command.LogoUrl!);
+        var address = new Address(command.Address1, command.Address2, command.City, command.Province, command.ZipCode);
+        var checkInOutTimes = new CheckInOutTimes(command.CheckInTime, command.CheckOutTime);
+        var itbisRate = new ItbisRate(command.ItbisRate);
+        var serviceChargeRate = new ServiceChargeRate(command.ServiceChargeRate);
+        
+        var hotel = new Hotel
+        (
+            command.LegalName,
+            command.CommercialName,
+            businessEmail,
+            businessPhoneNumber,
+            address,
+            checkInOutTimes,
+            itbisRate,
+            serviceChargeRate,
+            command.IsActive,
+            logoUrl,
+            command.DefaultCurrencyCode
+        );
+        
         try
         {
-            var hotel = new Hotel
-            (
-                command.LegalName,
-                command.CommercialName,
-                command.BusinessEmail,
-                command.BusinessPhoneNumber,
-                command.Address,
-                command.CheckInOutTimes,
-                command.ItbisRate,
-                command.ServiceChargeRate,
-                command.IsActive,
-                command.LogoUrl,
-                command.DefaultCurrencyCode
-            );
-            
             var response = await _repository.Create(hotel);
             await _unitOfWork.Persist();
             return response.Id;
