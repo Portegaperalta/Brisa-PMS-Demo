988232eabc095e44ab2af246295722d5d76d7c73
pablo <ortegaperaltap@gmail.com>
2026-04-05 21:39:28 -0400

    Add unit of work pattern

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelBrandInfo/UpdateHotelBrandInfoUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelBrandInfo/UpdateHotelBrandInfoUseCase.cs
@@ -1,3 +1,4 @@
+using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelBrandInfo;
@@ -5,10 +6,12 @@ namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelBrandInfo;
 public class UpdateHotelBrandInfoUseCase
 {
     private readonly IHotelsRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public UpdateHotelBrandInfoUseCase(IHotelsRepository repository)
+    public UpdateHotelBrandInfoUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(UpdateHotelBrandInfoCommand command)
@@ -17,10 +20,19 @@ public class UpdateHotelBrandInfoUseCase
         
         if (hotel == null)
             throw new ArgumentException($"Hotel with id {command.Id} not found");
-        
-        hotel.UpdateLegalName(command.LegalName);
-        hotel.UpdateCommercialName(command.CommercialName);
-        hotel.UpdateLogoUrl(command.LogoUrl!);
-        await _repository.Update(hotel);
+
+        try
+        {
+            hotel.UpdateLegalName(command.LegalName);
+            hotel.UpdateCommercialName(command.CommercialName);
+            hotel.UpdateLogoUrl(command.LogoUrl!);
+            await _repository.Update(hotel);
+            await _unitOfWork.Persist();
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
     }
 }
