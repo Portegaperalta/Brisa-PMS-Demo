6572ce620531400912c6b905f3b5aa64c598b58b
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:42:50 -0400

    Implement unit of work pattern

--- BrisaPMS.Application/UseCases/Hotels/Commands/ActivateHotel/ActivateHotelUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/ActivateHotel/ActivateHotelUseCase.cs
@@ -1,3 +1,4 @@
+using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.ActivateHotel;
@@ -5,20 +6,31 @@ namespace BrisaPMS.Application.UseCases.Hotels.Commands.ActivateHotel;
 public class ActivateHotelUseCase
 {
     private readonly IHotelsRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
     
-    public ActivateHotelUseCase(IHotelsRepository repository)
+    public ActivateHotelUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(ActivateHotelCommand command)
     {
-        var hotel = await _repository.GetById(command.Id);
+        var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
-            throw new ArgumentException($"Hotel with id {command.Id} not found");
+            throw new ArgumentException($"Hotel with id {command.HotelId} not found");
         
-        hotel.SetAsActive();
-        await _repository.Update(hotel);
+        try
+        {
+            hotel.SetAsActive();
+            await _repository.Update(hotel);
+            await _unitOfWork.Persist();
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
     }
 }
--- BrisaPMS.Application/UseCases/Hotels/Commands/DeactivateHotel/DeactivateHotelUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/DeactivateHotel/DeactivateHotelUseCase.cs
@@ -1,3 +1,4 @@
+using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.DeactivateHotel;
@@ -5,20 +6,31 @@ namespace BrisaPMS.Application.UseCases.Hotels.Commands.DeactivateHotel;
 public class DeactivateHotelUseCase
 {
     private readonly IHotelsRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public DeactivateHotelUseCase(IHotelsRepository repository)
+    public DeactivateHotelUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(DeactivateHotelCommand command)
     {
-        var hotel = await _repository.GetById(command.Id);
+        var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
-            throw new ArgumentException($"Hotel with id {command.Id} not found");
-        
-        hotel.SetAsInactive();
-        await _repository.Update(hotel);
+            throw new ArgumentException($"Hotel with id {command.HotelId} not found");
+
+        try
+        {
+            hotel.SetAsInactive();
+            await _repository.Update(hotel);
+            await _unitOfWork.Persist();
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
     }
 }
