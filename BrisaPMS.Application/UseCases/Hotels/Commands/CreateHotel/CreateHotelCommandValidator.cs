912c10154892a9abdc899efd18988959ded12ffe
pablo <ortegaperaltap@gmail.com>
2026-04-05 21:03:25 -0400

    Add more validation for CreateHotelCommand

--- BrisaPMS.Application/UseCases/Hotels/Commands/CreateHotel/CreateHotelCommandValidator.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/CreateHotel/CreateHotelCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System.Data;
+using FluentValidation;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.CreateHotel
 {
@@ -6,29 +7,50 @@ namespace BrisaPMS.Application.UseCases.Hotels.Commands.CreateHotel
     {
         public CreateHotelCommandValidator()
         {
-            RuleFor(p => p.LegalName)
-                .NotEmpty().WithMessage("The field Legal Name is required");
+            RuleFor(x => x.LegalName)
+                .NotEmpty().WithMessage("The field Legal Name is required")
+                .MaximumLength(250).WithMessage("The field Legal Name can't exceed 250 characters");
 
-            RuleFor(p => p.LegalName)
-                .NotEmpty().WithMessage("The field Commercial Name is required");
+            RuleFor(x => x.CommercialName)
+                .NotEmpty().WithMessage("The field Commercial Name is required")
+                .MaximumLength(250).WithMessage("The field Commercial Name can't exceed 250 characters");;
 
-            RuleFor(p => p.BusinessEmail)
-                 .NotEmpty().WithMessage("The field Business Email is required");
+            RuleFor(x => x.BusinessEmail)
+                 .NotEmpty().WithMessage("The field Business Email is required")
+                 .EmailAddress().WithMessage("The email must be a valid email address")
+                 .MaximumLength(254).WithMessage("The field Business Email can't exceed 254 characters");
 
-            RuleFor(p => p.BusinessPhoneNumber)
-                 .NotEmpty().WithMessage("The field Business Phone Number is required");
+            RuleFor(x => x.BusinessPhoneNumber)
+                 .NotEmpty().WithMessage("The field Business Phone Number is required")
+                 .MaximumLength(25).WithMessage("The field Business Phone Number can't exceed 25 characters");
 
-            RuleFor(p => p.Address)
+            RuleFor(x => x.Address1)
                  .NotEmpty().WithMessage("The field Address is required");
 
-            RuleFor(p => p.CheckInOutTimes)
-                 .NotEmpty().WithMessage("The field CheckInOutTimes is required");
+            RuleFor(x => x.City)
+                .NotEmpty().WithMessage("The field City is required");
 
-            RuleFor(p => p.ItbisRate)
-                 .NotEmpty().WithMessage("The field Itbis Rate is required");
+            RuleFor(x => x.Province)
+                .NotEmpty().WithMessage("The field Province is required");
 
-            RuleFor(p => p.ServiceChargeRate)
-                 .NotEmpty().WithMessage("The field Itbis Rate is required");
+            RuleFor(x => x.ZipCode)
+                .NotEmpty().WithMessage("The field ZipCode is required");
+
+            RuleFor(x => x.CheckInTime)
+                 .NotEmpty().WithMessage("The field Check-In Time is required");
+
+            RuleFor(x => x.CheckOutTime)
+                .NotEmpty().WithMessage("The field Check-Out Time is required");
+
+            RuleFor(x => x.ItbisRate)
+                 .NotEmpty().WithMessage("The field Itbis Rate is required")
+                 .GreaterThan(0).WithMessage("The field Itbis Rate can't be negative")
+                 .LessThan(100).WithMessage("The field Itbis Rate can't be greater than 100");
+
+            RuleFor(x => x.ServiceChargeRate)
+                 .NotEmpty().WithMessage("The field Service Charge Rate is required")
+                 .GreaterThan(0).WithMessage("The field Service Charge Rate can't be negative")
+                 .LessThan(100).WithMessage("The field Service Charge Rate can't be greater than 100");
         }
     }
 }
