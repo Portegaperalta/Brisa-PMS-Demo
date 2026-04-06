4f7924a042da80b332d11db261689c6e344a55b2
pablo <ortegaperaltap@gmail.com>
2026-04-05 21:52:35 -0400

    Inject Validator dependency and throw custom ValidationException if validation result is not valid

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelBrandInfo/UpdateHotelBrandInfoUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelBrandInfo/UpdateHotelBrandInfoUseCase.cs
@@ -1,5 +1,8 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Domain.Shared.ValueObjects;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelBrandInfo;
 
@@ -7,25 +10,39 @@ public class UpdateHotelBrandInfoUseCase
 {
     private readonly IHotelsRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<UpdateHotelBrandInfoCommand> _validator;
 
-    public UpdateHotelBrandInfoUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork)
+    public UpdateHotelBrandInfoUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork,
+        IValidator<UpdateHotelBrandInfoCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
 
     public async Task Handle(UpdateHotelBrandInfoCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
         var hotel = await _repository.GetById(command.Id);
         
-        if (hotel == null)
+        if (hotel is null)
             throw new ArgumentException($"Hotel with id {command.Id} not found");
 
         try
         {
             hotel.UpdateLegalName(command.LegalName);
             hotel.UpdateCommercialName(command.CommercialName);
-            hotel.UpdateLogoUrl(command.LogoUrl!);
+            
+            if (command.LogoUrl is not null)
+            {
+                var newLogo = new Url(command.LogoUrl);
+                hotel.UpdateLogoUrl(newLogo);
+            }
+            
             await _repository.Update(hotel);
             await _unitOfWork.Persist();
         }
