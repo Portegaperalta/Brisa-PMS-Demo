c2d9b7693c20e29dc36a03afee7d5e04633eaef2
pablo <ortegaperaltap@gmail.com>
2026-04-06 06:41:02 -0400

    Inject validator dependency and throw validation errors if validator fails, Add Address value object initialization with command values

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelAddressInfo/UpdateHotelAddressInfoUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelAddressInfo/UpdateHotelAddressInfoUseCase.cs
@@ -1,5 +1,8 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Domain.Shared.ValueObjects;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelAddressInfo;
 
@@ -7,24 +10,42 @@ public class UpdateHotelAddressInfoUseCase
 {
     private  readonly IHotelsRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<UpdateHotelAddressInfoCommand> _validator;
 
-    public UpdateHotelAddressInfoUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork)
+    public UpdateHotelAddressInfoUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork,
+        IValidator<UpdateHotelAddressInfoCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
 
     public async Task Handle(UpdateHotelAddressInfoCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
         var hotel = await _repository.GetById(command.Id);
         
         if (hotel is null)
             throw new ArgumentException($"Hotel with id {command.Id} not found");
+        
+        var newAddress = new Address
+            (
+                command.Address1, 
+                command.Address2, 
+                command.City, 
+                command.Province,
+                command.ZipCode
+            );
 
         try
         {
-            hotel.UpdateAddress(command.Address);
+            hotel.UpdateAddress(newAddress);
             await _repository.Update(hotel);
+            await _unitOfWork.Persist();
         }
         catch (Exception)
         {
