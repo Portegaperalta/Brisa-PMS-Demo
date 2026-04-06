7d0b65ad7d1409a7bb790d1adce7749def99d0d9
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:38:58 -0400

    Create Validators for Activate and Deactivate hotel commands

---
+++ BrisaPMS.Application/UseCases/Hotels/Commands/ActivateHotel/ActivateHotelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Hotels.Commands.ActivateHotel;
+
+public class ActivateHotelValidator : AbstractValidator<ActivateHotelCommand>
+{
+    public ActivateHotelValidator()
+    {
+        RuleFor(x => x.HotelId)
+            .NotEmpty().WithMessage("Field Hotel Id is required");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Hotels/Commands/DeactivateHotel/DeactivateHotelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Hotels.Commands.DeactivateHotel;
+
+public class DeactivateHotelValidator : AbstractValidator<DeactivateHotelCommand>
+{
+    public DeactivateHotelValidator()
+    {
+        RuleFor(x => x.HotelId)
+            .NotEmpty().WithMessage("Field Hotel Id is required");
+    }
+}
