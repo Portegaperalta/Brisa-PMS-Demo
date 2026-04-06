727a8bda8edc96ab40a2b3c4590c095712813b5e
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:22:23 -0400

    Inject validator dependency and throw validation errors if validator fails, Add DefaultCurrency value object initialization with command values

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelDefaultCurrency/UpdateHotelDefaultCurrencyUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelDefaultCurrency/UpdateHotelDefaultCurrencyUseCase.cs
@@ -1,5 +1,9 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Domain.Shared.Enums;
+using BrisaPMS.Domain.Shared.ValueObjects;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelDefaultCurrency;
 
@@ -7,23 +11,33 @@ public class UpdateHotelDefaultCurrencyUseCase
 {
     private readonly IHotelsRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<UpdateHotelDefaultCurrencyCommand> _validator;
 
-    public UpdateHotelDefaultCurrencyUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork)
+    public UpdateHotelDefaultCurrencyUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork,
+        IValidator<UpdateHotelDefaultCurrencyCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
 
     public async Task Handle(UpdateHotelDefaultCurrencyCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
         var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
             throw new ArgumentException($"Hotel with id {command.HotelId} not found");
-
+        
+        var newDefaultCurrencyCode = (CurrencyCode)Enum.Parse(typeof(CurrencyCode), command.DefaultCurrencyCode);
+        
         try
         {
-            hotel.UpdateDefaultCurrencyCode(command.CurrencyCode);
+            hotel.UpdateDefaultCurrencyCode(newDefaultCurrencyCode);
             await _repository.Update(hotel);
             await _unitOfWork.Persist();
         }
