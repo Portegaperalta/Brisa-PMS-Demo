f03dc697abaa2ae12ac42a09668eed223fcb3d74
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:32:48 -0400

    Add value object initialization with command values

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelRates/UpdateHotelRatesUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelRates/UpdateHotelRatesUseCase.cs
@@ -1,25 +1,41 @@
+using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Domain.Billing;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelRates;
 
 public class UpdateHotelRatesUseCase
 {
     private readonly IHotelsRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public UpdateHotelRatesUseCase(IHotelsRepository repository)
+    public UpdateHotelRatesUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(UpdateHotelRatesCommand command)
     {
-        var hotel = await _repository.GetById(command.Id);
+        var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
-            throw new ArgumentException($"Hotel with id {command.Id} not found");
+            throw new ArgumentException($"Hotel with id {command.HotelId} not found");
         
-        hotel.UpdateItbisRate(command.ItbisRate);
-        hotel.UpdateServiceChargeRate(command.ServiceChargeRate);
-        await _repository.Update(hotel);
+        var newItbisRate = new ItbisRate(command.ItbisRate);
+        var newServiceChargeRate = new ServiceChargeRate(command.ServiceChargeRate);
+
+        try
+        {
+            hotel.UpdateItbisRate(newItbisRate);
+            hotel.UpdateServiceChargeRate(newServiceChargeRate);
+            await _repository.Update(hotel);
+            await _unitOfWork.Persist();
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
     }
 }
