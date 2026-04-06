65f0630bf4090ee0e7de10d864bd981da71e759a
pablo <ortegaperaltap@gmail.com>
2026-04-05 21:02:52 -0400

    Change value objects in command to primitive types

--- BrisaPMS.Application/UseCases/Hotels/Commands/CreateHotel/CreateHotelCommand.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/CreateHotel/CreateHotelCommand.cs
@@ -8,13 +8,18 @@ public class CreateHotelCommand
 {
     public required string LegalName { get; set; }
     public required string CommercialName { get; set; }
-    public Url? LogoUrl { get; set; } = null;
-    public required Email BusinessEmail { get; set; }
-    public required PhoneNumber BusinessPhoneNumber { get; set; }
-    public required Address Address { get; set; }
-    public required CheckInOutTimes CheckInOutTimes { get; set; }
+    public string? LogoUrl { get; set; } = null;
+    public required string BusinessEmail { get; set; }
+    public required string BusinessPhoneNumber { get; set; }
+    public required string Address1 { get; set; }
+    public string? Address2 { get; set; }
+    public required string City { get; set; }
+    public required string Province { get; set; }
+    public required string ZipCode { get; set; }
+    public required DateTime CheckInTime { get; set; }
+    public required DateTime CheckOutTime { get; set; }
     public CurrencyCode DefaultCurrencyCode { get; set; }
-    public required ItbisRate ItbisRate { get; set; }
-    public required ServiceChargeRate ServiceChargeRate { get; set; }
+    public required decimal ItbisRate { get; set; }
+    public required decimal ServiceChargeRate { get; set; }
     public bool IsActive { get; set; }
 }
