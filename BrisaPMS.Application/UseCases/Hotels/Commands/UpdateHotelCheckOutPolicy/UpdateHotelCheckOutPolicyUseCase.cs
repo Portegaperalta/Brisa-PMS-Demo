a21ae4769f50a00958719fd7a944d688526df55c
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:04:39 -0400

    Inject validator dependency and throw validation errors if validator fails, Add CheckInOutTimes value object initialization with command values

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelCheckOutPolicy/UpdateHotelCheckOutPolicyUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelCheckOutPolicy/UpdateHotelCheckOutPolicyUseCase.cs
@@ -1,5 +1,8 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Domain.Shared.ValueObjects;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelCheckOutPolicy;
 
@@ -7,23 +10,33 @@ public class UpdateHotelCheckOutPolicyUseCase
 {
     private readonly IHotelsRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<UpdateHotelCheckOutPolicyCommand> _validator;
     
-    public UpdateHotelCheckOutPolicyUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork)
+    public UpdateHotelCheckOutPolicyUseCase(IHotelsRepository repository, IUnitOfWork unitOfWork,
+        IValidator<UpdateHotelCheckOutPolicyCommand> validator)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
+        _validator = validator;
     }
 
     public async Task Handle(UpdateHotelCheckOutPolicyCommand command)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+        
+        if (validationResult.IsValid is not true)
+            throw new ValidationException(validationResult);
+        
         var hotel = await _repository.GetById(command.Id);
         
         if (hotel is null)
             throw new ArgumentException($"Hotel with id {command.Id} not found");
-
+        
+        var newCheckOutPolicy = new CheckInOutTimes(command.CheckInTime, command.CheckOutTime);
+        
         try
         {
-            hotel.UpdateCheckInOutTimes(command.CheckInOutTimes);
+            hotel.UpdateCheckInOutTimes(newCheckOutPolicy);
             await _repository.Update(hotel);
             await _unitOfWork.Persist();
         }
