126502f81c7eb6db3f1623e74452389e5d71fe71
Pablo <ortegaperaltap@gmail.com>
2026-04-06 07:13:41 -0400

    Implement unit of work pattern

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelDefaultCurrency/UpdateHotelDefaultCurrencyUseCase.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelDefaultCurrency/UpdateHotelDefaultCurrencyUseCase.cs
@@ -1,3 +1,4 @@
+using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
 
 namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelDefaultCurrency;
@@ -5,20 +6,31 @@ namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelDefaultCurren
 public class UpdateHotelDefaultCurrencyUseCase
 {
     private readonly IHotelsRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public UpdateHotelDefaultCurrencyUseCase(IHotelsRepository repository)
+    public UpdateHotelDefaultCurrencyUseCase(IHotelsRepository repository,  IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task Handle(UpdateHotelDefaultCurrencyCommand command)
     {
-        var hotel = await _repository.GetById(command.Id);
+        var hotel = await _repository.GetById(command.HotelId);
         
         if (hotel is null)
-            throw new ArgumentException($"Hotel with id {command.Id} not found");
-        
-        hotel.UpdateDefaultCurrencyCode(command.CurrencyCode);
-        await _repository.Update(hotel);
+            throw new ArgumentException($"Hotel with id {command.HotelId} not found");
+
+        try
+        {
+            hotel.UpdateDefaultCurrencyCode(command.CurrencyCode);
+            await _repository.Update(hotel);
+            await _unitOfWork.Persist();
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
     }
 }
