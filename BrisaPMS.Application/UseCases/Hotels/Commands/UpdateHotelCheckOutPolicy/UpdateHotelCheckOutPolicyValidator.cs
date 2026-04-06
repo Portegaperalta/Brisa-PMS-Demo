8c0a36c22d4b0f330ee6081b41689754a742eb58
Pablo <ortegaperaltap@gmail.com>
2026-04-06 06:59:51 -0400

    Create validator for UpdateHotelCheckOutPolicyCommand

---
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelCheckOutPolicy/UpdateHotelCheckOutPolicyValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelCheckOutPolicy;
+
+public class UpdateHotelCheckOutPolicyValidator : AbstractValidator<UpdateHotelCheckOutPolicyCommand>
+{
+    public UpdateHotelCheckOutPolicyValidator()
+    {
+        RuleFor(x => x.CheckInTime)
+            .NotEmpty().WithMessage("The field  CheckInTime is required");
+        
+        RuleFor(x => x.CheckOutTime)
+            .NotEmpty().WithMessage("The field  CheckOutTime is required");
+    }
+}
