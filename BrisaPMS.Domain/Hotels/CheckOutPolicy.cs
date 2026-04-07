142d7eb9c79fec9a3809e404e1ce0700292074cd
pablo <ortegaperaltap@gmail.com>
2026-04-07 11:55:37 -0400

    Remove time comparison validators

--- BrisaPMS.Domain/Hotels/CheckOutPolicy.cs
+++ BrisaPMS.Domain/Hotels/CheckOutPolicy.cs
@@ -9,15 +9,6 @@ public record CheckOutPolicy
 
     public CheckOutPolicy(TimeOnly checkInTime, TimeOnly checkOutTime)
     {
-        if (checkInTime > checkOutTime)
-            throw new BusinessRuleException("Check-In time can't be greater than Check-Out time.");
-        
-        if (checkOutTime < checkInTime)
-            throw new BusinessRuleException("Check-Out time can't be less than Check-In time.");
-        
-        if (checkInTime ==  checkOutTime)
-            throw new BusinessRuleException("Check-In time can't be the same as Check-Out time.");
-        
         CheckInTime = checkInTime;
         CheckOutTime = checkOutTime;
     }
