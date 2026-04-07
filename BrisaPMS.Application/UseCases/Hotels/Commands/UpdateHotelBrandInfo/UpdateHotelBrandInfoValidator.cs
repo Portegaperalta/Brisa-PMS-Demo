d2a1f1605e8a9ea20e28becf489b38061cc98aee
Pablo <ortegaperaltap@gmail.com>
2026-04-07 07:42:57 -0400

    Add string length validation to logo url

--- BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelBrandInfo/UpdateHotelBrandInfoValidator.cs
+++ BrisaPMS.Application/UseCases/Hotels/Commands/UpdateHotelBrandInfo/UpdateHotelBrandInfoValidator.cs
@@ -8,16 +8,17 @@ public class UpdateHotelBrandInfoValidator : AbstractValidator<UpdateHotelBrandI
     {
         RuleFor(x => x.HotelId)
             .NotEmpty().WithMessage("Field Id is required");
-        
+
         RuleFor(x => x.LegalName)
             .NotEmpty().WithMessage("Field LegalName is required")
             .MaximumLength(250).WithMessage("Field Legal Name must not exceed 250 characters");
-        
+
         RuleFor(x => x.CommercialName)
             .NotEmpty().WithMessage("Field Commercial Name is required")
             .MaximumLength(250).WithMessage("Field Commercial Name must not exceed 250 characters");
-        
+
         RuleFor(x => x.LogoUrl)
+            .MaximumLength(2048).WithMessage("Field Logo Url must not exceed 250 characters")
             .Must(url => Uri.TryCreate(url, UriKind.Absolute, out var uri)
             && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
             .WithMessage("A valid HTTP/HTTPS URL is required");
