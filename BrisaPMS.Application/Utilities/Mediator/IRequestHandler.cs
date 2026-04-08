10971fc16e5867da2002638be9df1001fa2ecfd9
pablo <ortegaperaltap@gmail.com>
2026-04-08 13:08:53 -0400

    Create IRequestHandler

---
+++ BrisaPMS.Application/Utilities/Mediator/IRequestHandler.cs
@@ -0,0 +1,7 @@
+namespace BrisaPMS.Application.Utilities.Mediator;
+
+public interface IRequestHandler<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+{
+    Task<TResponse> Handle(TRequest request);
+}
