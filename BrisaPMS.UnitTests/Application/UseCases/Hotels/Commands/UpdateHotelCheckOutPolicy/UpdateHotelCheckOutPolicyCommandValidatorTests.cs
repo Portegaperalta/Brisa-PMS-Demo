8047b6c9d2f08e8a4f8a6f6a22572427e5fcfeb6
Pablo <ortegaperaltap@gmail.com>
2026-04-07 22:03:43 -0400

    Create unit tests for UpdateHotelCheckOutPolicyCommandValidator

---
+++ BrisaPMS.UnitTests/Application/UseCases/Hotels/Commands/UpdateHotelCheckOutPolicy/UpdateHotelCheckOutPolicyCommandValidatorTests.cs
@@ -0,0 +1,43 @@
+using BrisaPMS.Application.UseCases.Hotels.Commands.UpdateHotelCheckOutPolicy;
+using FluentValidation.TestHelper;
+
+namespace BrisaPMS.UnitTests.Application.UseCases.Hotels.Commands.UpdateHotelCheckOutPolicy;
+
+public class UpdateHotelCheckOutPolicyCommandValidatorTests
+{
+  private readonly UpdateHotelCheckOutPolicyCommandValidator _validator;
+
+  public UpdateHotelCheckOutPolicyCommandValidatorTests()
+  {
+    _validator = new UpdateHotelCheckOutPolicyCommandValidator();
+  }
+
+  [Fact]
+  public void Validator_HasErrors_WhenRequiredFieldsAreEmpty()
+  {
+    // Arrange
+    var command = CreateUpdateHotelCheckOutPolicyCommand(Guid.Empty, default, default);
+
+    // Act
+    var result = _validator.TestValidate(command);
+
+    // Assert
+    result.ShouldHaveValidationErrorFor(x => x.HotelId);
+    result.ShouldHaveValidationErrorFor(x => x.CheckInTime);
+    result.ShouldHaveValidationErrorFor(x => x.CheckOutTime);
+  }
+
+  // Helper methods
+  private static UpdateHotelCheckOutPolicyCommand CreateUpdateHotelCheckOutPolicyCommand(
+      Guid hotelId,
+      TimeOnly checkInTime,
+      TimeOnly checkOutTime)
+  {
+    return new UpdateHotelCheckOutPolicyCommand
+    {
+      HotelId = hotelId,
+      CheckInTime = checkInTime,
+      CheckOutTime = checkOutTime
+    };
+  }
+}
