76b3648078bb7d2fe43a5d1e10dd05929129a15c
Pablo <ortegaperaltap@gmail.com>
2026-04-08 19:35:04 -0400

    Centralize validations inside the Mediator

--- BrisaPMS.Application/Utilities/Mediator/SimpleMediator.cs
+++ BrisaPMS.Application/Utilities/Mediator/SimpleMediator.cs
@@ -1,4 +1,7 @@
 using BrisaPMS.Application.Exceptions;
+using FluentValidation;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
+using ValidationResult = FluentValidation.Results.ValidationResult;
 
 namespace BrisaPMS.Application.Utilities.Mediator;
 
@@ -6,13 +9,29 @@ public class SimpleMediator : IMediator
 {
     private readonly IServiceProvider _serviceProvider;
     
-    public SimpleMediator(IServiceProvider serviceProvider)
-    {
-        _serviceProvider = serviceProvider;
-    }
+    public SimpleMediator(IServiceProvider serviceProvider) { _serviceProvider = serviceProvider; }
     
     public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
     {
+        var validatorType = typeof(IValidator<>).MakeGenericType(request.GetType());
+        
+        var validator = _serviceProvider.GetService(validatorType);
+
+        if (validator is not null)
+        {
+            var validateMethod = validatorType.GetMethod("ValidateAsync");
+            var validateTask = (Task)validateMethod!.Invoke(validator, 
+                    new object[] { request, CancellationToken.None })!;
+            
+            await  validateTask.ConfigureAwait(false);
+
+            var result = validateTask.GetType().GetProperty("Result");
+            var validationResult = (ValidationResult)result!.GetValue(validateTask)!;
+            
+            if (validationResult.IsValid is not true)
+                throw new ValidationException(validationResult);
+        }
+        
         var useCaseType = typeof(IRequestHandler<,>)
             .MakeGenericType(request.GetType(), typeof(TResponse));
         
