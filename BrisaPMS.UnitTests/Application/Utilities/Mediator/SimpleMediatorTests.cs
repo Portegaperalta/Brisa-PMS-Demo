553917ccad13b30086919a83f974d4849d565266
Pablo <ortegaperaltap@gmail.com>
2026-04-08 20:46:52 -0400

    Refactor FakeRequest and FakeHandler to use string return type and create test method to validate Send() throws ValidationException when command is not valid.

--- BrisaPMS.UnitTests/Application/Utilities/Mediator/SimpleMediatorTests.cs
+++ BrisaPMS.UnitTests/Application/Utilities/Mediator/SimpleMediatorTests.cs
@@ -1,19 +1,32 @@
 using BrisaPMS.Application.Exceptions;
 using BrisaPMS.Application.Utilities.Mediator;
 using FluentAssertions;
+using FluentValidation;
 using NSubstitute;
+using ValidationException = BrisaPMS.Application.Exceptions.ValidationException;
 
 namespace BrisaPMS.UnitTests.Application.Utilities.Mediator;
 
 public class SimpleMediatorTests
 {
-    public class FakeRequest : IRequest<bool> {}
-
-    public class FakeHandler : IRequestHandler<FakeRequest, bool>
+    public class FakeRequest : IRequest<string>
     {
-        public Task<bool> Handle(FakeRequest request)
+        public required string Name { get; set; }
+    }
+
+    public class FakeHandler : IRequestHandler<FakeRequest, string>
+    {
+        public Task<string> Handle(FakeRequest request)
         {
-            return Task.FromResult(true);
+            return Task.FromResult("Correct response");
+        }
+    }
+
+    public class FakeValidatorRequest : AbstractValidator<FakeRequest>
+    {
+        public FakeValidatorRequest()
+        {
+            RuleFor(x => x.Name).NotEmpty();
         }
     }
 
@@ -21,12 +34,12 @@ public class SimpleMediatorTests
     public async Task Send_CallsHandler()
     {
         // Arrange
-        var request = new FakeRequest();
-        var useCaseMock = Substitute.For<IRequestHandler<FakeRequest, bool>>();
+        var request = new FakeRequest() {Name = "Test name"};
+        var useCaseMock = Substitute.For<IRequestHandler<FakeRequest, string>>();
         var serviceProviderMock = Substitute.For<IServiceProvider>();
         
         serviceProviderMock
-            .GetService(typeof(IRequestHandler<FakeRequest, bool>))
+            .GetService(typeof(IRequestHandler<FakeRequest, string>))
             .Returns(useCaseMock);
         
         var mediator = new SimpleMediator(serviceProviderMock);
@@ -42,7 +55,7 @@ public class SimpleMediatorTests
     public async Task Send_ThrowsMediatorException_WhenHandlerIsNotRegistered()
     {
         // Arrange
-        var request = new FakeRequest();
+        var request = new FakeRequest() {Name =  "Test name"};
         var serviceProviderMock = Substitute.For<IServiceProvider>();
         var mediator = new SimpleMediator(serviceProviderMock);
         
@@ -51,5 +64,26 @@ public class SimpleMediatorTests
         
         // Assert
         await act.Should().ThrowAsync<MediatorException>();
+    }
+
+    [Fact]
+    public async Task Send_ThrowsValidationException_WhenCommandIsNotValid()
+    {
+        // Arrange
+        var request = new FakeRequest() {Name = ""};
+        var serviceProviderMock = Substitute.For<IServiceProvider>();
+        var validator = new FakeValidatorRequest();
+
+        serviceProviderMock
+            .GetService(typeof(IValidator<FakeRequest>))
+            .Returns(validator);
+        
+        var mediator = new SimpleMediator(serviceProviderMock);
+        
+        // Act
+        var act = async () => await mediator.Send(request);
+        
+        // Assert
+        await act.Should().ThrowAsync<ValidationException>();
     }
 }
