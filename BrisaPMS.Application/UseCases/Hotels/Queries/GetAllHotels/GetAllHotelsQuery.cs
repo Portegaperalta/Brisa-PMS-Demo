ec650ad914c02b9ef884bb9211e2316efc2bf06b
Pablo <ortegaperaltap@gmail.com>
2026-04-09 07:46:26 -0400

    Create GetAllHotels query

---
+++ BrisaPMS.Application/UseCases/Hotels/Queries/GetAllHotels/GetAllHotelsQuery.cs
@@ -0,0 +1,7 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Hotels.Queries.GetAllHotels;
+
+public class GetAllHotelsQuery : IRequest<List<HotelDto>>
+{
+}
