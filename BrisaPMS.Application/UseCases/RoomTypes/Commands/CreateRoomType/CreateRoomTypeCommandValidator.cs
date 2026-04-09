bd33cb97675904c71fc607a195027279f0dc3b92
pablo <ortegaperaltap@gmail.com>
2026-04-09 18:34:34 -0400

    Add number limit validation

--- BrisaPMS.Application/UseCases/RoomTypes/Commands/CreateRoomType/CreateRoomTypeCommandValidator.cs
+++ BrisaPMS.Application/UseCases/RoomTypes/Commands/CreateRoomType/CreateRoomTypeCommandValidator.cs
@@ -16,11 +16,13 @@ public class CreateRoomTypeCommandValidator : AbstractValidator<CreateRoomTypeCo
         
         RuleFor(x => x.BaseRate)
             .NotEmpty().WithMessage("The field BaseRate is required.")
-            .GreaterThanOrEqualTo(0).WithMessage("The field BaseRate can't be negative.");
+            .GreaterThanOrEqualTo(0).WithMessage("The field BaseRate can't be negative.")
+            .LessThanOrEqualTo(100).WithMessage("The field Base Rate can't be greater than 100");
         
         RuleFor(x => x.TotalBeds)
             .NotEmpty().WithMessage("The field Total Beds is required.")
-            .GreaterThanOrEqualTo(1).WithMessage("The field Total Beds must be  greater than or equal to 1.");
+            .GreaterThanOrEqualTo(1).WithMessage("The field Total Beds must be  greater than or equal to 1.")
+            .LessThanOrEqualTo(100).WithMessage("The field Total Beds can't be greater than 100.");
         
         RuleFor(x => x.BedType)
             .NotEmpty().WithMessage("The field Bed Type is required.")
@@ -29,10 +31,12 @@ public class CreateRoomTypeCommandValidator : AbstractValidator<CreateRoomTypeCo
         
         RuleFor(x => x.MaxOccupancyAdults)
             .NotEmpty().WithMessage("The field Max Occupancy Adults is required.")
-            .GreaterThanOrEqualTo(1).WithMessage("The field Max Occupancy Adults  must be greater than or equal to 1.");
+            .GreaterThanOrEqualTo(1).WithMessage("The field Max Occupancy Adults  must be greater than or equal to 1.")
+            .LessThanOrEqualTo(16).WithMessage("The field Max Occupancy Adults can't be greater than 16.");
         
         RuleFor(x => x.MaxOccupancyChildren)
             .NotEmpty().WithMessage("The field Max Occupancy Children is required.")
-            .GreaterThanOrEqualTo(0).WithMessage("the field Max  Occupancy Children can't be negative.");
+            .GreaterThanOrEqualTo(0).WithMessage("the field Max  Occupancy Children can't be negative.")
+            .LessThanOrEqualTo(10).WithMessage("the field Max Occupancy Children can't be greater than 10.");
     }
 }
