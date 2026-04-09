fdb5aa5af6159237ca2b7a50bd78e31c36aa1b2d
pablo <ortegaperaltap@gmail.com>
2026-04-09 13:43:38 -0400

    Change command fields with type enum to string

--- BrisaPMS.Application/UseCases/Rooms/Commands/CreateRoom/CreateRoomCommand.cs
+++ BrisaPMS.Application/UseCases/Rooms/Commands/CreateRoom/CreateRoomCommand.cs
@@ -9,6 +9,6 @@ public class CreateRoomCommand : IRequest<Guid>
     public required Guid RoomTypeId { get; set; }
     public required string Number { get; set; }
     public required int Floor { get; set; }
-    public required RoomAvailabilityStatus AvailabilityStatus { get; set; }
-    public required RoomHygieneStatus HygieneStatus { get; set; }
+    public required string AvailabilityStatus { get; set; }
+    public required string HygieneStatus { get; set; }
 }
