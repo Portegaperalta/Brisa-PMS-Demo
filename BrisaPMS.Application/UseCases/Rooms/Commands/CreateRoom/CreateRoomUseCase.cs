11036ae3eb7ea63c9cbfc95348b7bd456deb909c
pablo <ortegaperaltap@gmail.com>
2026-04-09 20:11:53 -0400

    Inject roomType repository dependency and validate if room type exists before creating room entity

--- BrisaPMS.Application/UseCases/Rooms/Commands/CreateRoom/CreateRoomUseCase.cs
+++ BrisaPMS.Application/UseCases/Rooms/Commands/CreateRoom/CreateRoomUseCase.cs
@@ -10,21 +10,28 @@ public class CreateRoomUseCase : IRequestHandler<CreateRoomCommand, Guid>
 {
     private readonly IRoomsRepository _roomsRepository;
     private readonly IHotelsRepository _hotelsRepository;
+    private readonly IRoomTypesRepository _roomTypesRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public CreateRoomUseCase(IRoomsRepository roomsRepository, IHotelsRepository hotelsRepository,IUnitOfWork unitOfWork)
+    public CreateRoomUseCase(IRoomsRepository roomsRepository, IHotelsRepository hotelsRepository,
+        IRoomTypesRepository roomTypesRepository,IUnitOfWork unitOfWork)
     {
         _roomsRepository = roomsRepository;
         _hotelsRepository = hotelsRepository;
+        _roomTypesRepository = roomTypesRepository;
         _unitOfWork = unitOfWork;
     }
 
     public async Task<Guid> Handle(CreateRoomCommand command)
     {
         var hotel = await _hotelsRepository.GetById(command.HotelId);
+        var roomType = await _roomTypesRepository.GetById(command.RoomTypeId);
 
         if (hotel is null)
             throw new NotFoundException("Hotel", command.HotelId);
+        
+        if (roomType is null)
+            throw new NotFoundException("Room Type", command.RoomTypeId);
         
         var availabilityStatus = Enum.Parse<RoomAvailabilityStatus>(command.AvailabilityStatus);
         var hygieneStatus = Enum.Parse<RoomHygieneStatus>(command.HygieneStatus);
