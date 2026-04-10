75eadd48adfb3343ceaf8ce1cded7109ec9dda00
pablo <ortegaperaltap@gmail.com>
2026-04-09 20:03:34 -0400

    Create use case for CreateRoom

--- BrisaPMS.Application/UseCases/Rooms/Commands/CreateRoom/CreateRoomUseCase.cs
+++ BrisaPMS.Application/UseCases/Rooms/Commands/CreateRoom/CreateRoomUseCase.cs
@@ -1,5 +1,6 @@
 using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
 using BrisaPMS.Application.Utilities.Mediator;
 using BrisaPMS.Domain.Rooms;
 
@@ -7,30 +8,40 @@ namespace BrisaPMS.Application.UseCases.Rooms.Commands.CreateRoom;
 
 public class CreateRoomUseCase : IRequestHandler<CreateRoomCommand, Guid>
 {
-    private readonly IRoomsRepository _repository;
+    private readonly IRoomsRepository _roomsRepository;
+    private readonly IHotelsRepository _hotelsRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public CreateRoomUseCase(IRoomsRepository repository, IUnitOfWork unitOfWork)
+    public CreateRoomUseCase(IRoomsRepository roomsRepository, IHotelsRepository hotelsRepository,IUnitOfWork unitOfWork)
     {
-        _repository = repository;
+        _roomsRepository = roomsRepository;
+        _hotelsRepository = hotelsRepository;
         _unitOfWork = unitOfWork;
     }
 
     public async Task<Guid> Handle(CreateRoomCommand command)
     {
+        var hotel = await _hotelsRepository.GetById(command.HotelId);
+
+        if (hotel is null)
+            throw new NotFoundException("Hotel", command.HotelId);
+        
+        var availabilityStatus = Enum.Parse<RoomAvailabilityStatus>(command.AvailabilityStatus);
+        var hygieneStatus = Enum.Parse<RoomHygieneStatus>(command.HygieneStatus);
+        
         var room = new Room
         (
             command.HotelId,
             command.RoomTypeId,
             command.Number,
             command.Floor,
-            command.AvailabilityStatus,
-            command.HygieneStatus
+            availabilityStatus,
+            hygieneStatus
         );
         
         try
         {
-            var response = await _repository.Create(room);
+            var response = await _roomsRepository.Create(room);
             await _unitOfWork.Persist();
             return response.Id;
         }
