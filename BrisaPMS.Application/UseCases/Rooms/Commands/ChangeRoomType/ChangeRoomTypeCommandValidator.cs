af476187f638e85f9fda2f9399a69378a007c65e
Pablo <ortegaperaltap@gmail.com>
2026-04-10 19:16:35 -0400

    Create ChangeRoomType command and validator

---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/ChangeRoomType/ChangeRoomTypeCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.ChangeRoomType;
+
+public class ChangeRoomTypeCommandValidator : AbstractValidator<ChangeRoomTypeCommand>
+{
+    public ChangeRoomTypeCommandValidator()
+    {
+        RuleFor(x => x.RoomId)
+            .NotEmpty().WithMessage("The field RoomId is required.");
+        
+        RuleFor(x => x.RoomTypeId)
+            .NotEmpty().WithMessage("The field RoomTypeId is required.");
+    }
+}
