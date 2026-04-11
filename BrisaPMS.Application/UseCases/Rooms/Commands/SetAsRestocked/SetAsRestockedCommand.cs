4ac288c6ca5904bbe0852b380002ae5d9ae549a0
Pablo <ortegaperaltap@gmail.com>
2026-04-10 22:43:47 -0400

    Create SetAsRestocked command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/SetAsRestocked/SetAsRestockedCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.SetAsRestocked;
+
+public class SetAsRestockedCommand : IRequest<bool>
+{
+    public required Guid RoomId { get; set; }
+}
