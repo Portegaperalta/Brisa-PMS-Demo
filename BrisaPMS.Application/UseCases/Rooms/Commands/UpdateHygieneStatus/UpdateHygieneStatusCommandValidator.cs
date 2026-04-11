ac4a225915aac19eb0008ea5c67ef73e6f5c78ae
Pablo <ortegaperaltap@gmail.com>
2026-04-10 21:16:27 -0400

    Create UpdateHygieneStatus command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/UpdateHygieneStatus/UpdateHygieneStatusCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using BrisaPMS.Domain.Rooms;
+using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.UpdateHygieneStatus;
+
+public class UpdateHygieneStatusCommandValidator : AbstractValidator<UpdateHygieneStatusCommand>
+{
+    public UpdateHygieneStatusCommandValidator()
+    {
+        RuleFor(x => x.RoomId)
+            .NotEmpty().WithMessage("The field RoomId is required");
+        
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("The field UserId is required");
+        
+        RuleFor(x => x.HygieneStatus)
+            .NotEmpty().WithMessage("The field Hygiene Status is required")
+            .MaximumLength(11).WithMessage("The field Hygiene Status can't exceed 11 characters")
+            .Must(x => Enum.IsDefined(typeof(RoomHygieneStatus), x))
+            .WithMessage("Hygiene status not supported");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/UpdateHygieneStatus/UpdateHygieneStatusUseCase.cs
@@ -0,0 +1,48 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.Rooms;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.UpdateHygieneStatus;
+
+public class UpdateHygieneStatusUseCase : IRequestHandler<UpdateHygieneStatusCommand, bool>
+{
+    private readonly IRoomsRepository _roomsRepository;
+    private readonly IUnitOfWork  _unitOfWork;
+
+    public UpdateHygieneStatusUseCase(IRoomsRepository roomsRepository, IUnitOfWork unitOfWork)
+    {
+        _roomsRepository = roomsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateHygieneStatusCommand command)
+    {
+        var room = await _roomsRepository.GetById(command.RoomId);
+        var newHygieneStatus = Enum.Parse<RoomHygieneStatus>(command.HygieneStatus);
+        
+        if (room is null)
+            throw new NotFoundException("Room", command.RoomId);
+        
+        room.UpdateHygieneStatus(newHygieneStatus);
+            
+        if (newHygieneStatus is RoomHygieneStatus.Clean)
+        {
+            room.UpdateLastCleanedAt();
+            room.UpdateLastCleanedBy(command.UserId);
+        }
+
+        try
+        {
+            await _roomsRepository.Update(room);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
