5daffcbe5f5b42914703f2615aa2a8febab9cb06
Pablo <ortegaperaltap@gmail.com>
2026-04-11 10:02:16 -0400

    Create RoomDto

---
+++ BrisaPMS.Application/UseCases/Rooms/Shared/RoomDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BrisaPMS.Application.UseCases.Rooms.Shared;
+
+public class RoomDto
+{
+    public required Guid Id { get; init; }
+    public required Guid HotelId { get; init;}
+    public required string Number { get; init; }
+    public required int Floor { get; init; }
+    public required string AvailabilityStatus { get; init; }
+    public required string HygieneStatus { get; init; }
+    public DateTime? LastCleanedAt { get; init; }
+    public Guid? LastCleanedBy { get; init; }
+    public required bool NeedsRestocking { get; init; }
+}
