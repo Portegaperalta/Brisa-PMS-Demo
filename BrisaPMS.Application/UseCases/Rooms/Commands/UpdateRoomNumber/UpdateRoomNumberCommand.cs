042707d50cc2de20c14faac2a50a5ce72ba72746
Pablo <ortegaperaltap@gmail.com>
2026-04-11 09:31:06 -0400

    Create UpdateRoomNumber command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/UpdateRoomNumber/UpdateRoomNumberCommand.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.UpdateRoomNumber;
+
+public class UpdateRoomNumberCommand : IRequest<bool>
+{
+    public required Guid RoomId { get; set; }
+    public required string Number { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/UpdateRoomNumber/UpdateRoomNumberUseCase.cs
@@ -0,0 +1,40 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.UpdateRoomNumber;
+
+public class UpdateRoomNumberUseCase : IRequestHandler<UpdateRoomNumberCommand, bool>
+{
+    private readonly IRoomsRepository _roomsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateRoomNumberUseCase(IRoomsRepository roomsRepository, IUnitOfWork unitOfWork)
+    {
+        _roomsRepository = roomsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateRoomNumberCommand command)
+    {
+        var room = await _roomsRepository.GetById(command.RoomId);
+        
+        if (room is null)
+            throw new NotFoundException("Room", command.RoomId);
+        
+        room.UpdateNumber(command.Number);
+
+        try
+        {
+            await _roomsRepository.Update(room);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine();
+            throw;
+        }
+    }
+}
