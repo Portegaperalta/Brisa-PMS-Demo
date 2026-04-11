d1c999b50ff657c9c6d058f0bf6fee9c5fc272ad
Pablo <ortegaperaltap@gmail.com>
2026-04-10 20:11:09 -0400

    Create UpdateAvailabilityStatus command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/UpdateAvailabilityStatus/UpdateAvailabilityStatusCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using BrisaPMS.Domain.Rooms;
+using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.UpdateAvailabilityStatus;
+
+public class UpdateAvailabilityStatusCommandValidator : AbstractValidator<UpdateAvailabilityStatusCommand>
+{
+    public UpdateAvailabilityStatusCommandValidator()
+    {
+        RuleFor(x => x.RoomId)
+            .NotEmpty().WithMessage("The field RoomId is required.");
+        
+        RuleFor(x => x.AvailabilityStatus)
+            .NotEmpty().WithMessage("The field Availability Status is required.")
+            .MaximumLength(11).WithMessage("The field Availability Status can't exceed 11 characters.")
+            .Must(x => Enum.IsDefined(typeof(RoomAvailabilityStatus), x))
+            .WithMessage("Availability status not supported.");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/UpdateAvailabilityStatus/UpdateAvailabilityStatusUseCase.cs
@@ -0,0 +1,43 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.Rooms;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.UpdateAvailabilityStatus;
+
+public class UpdateAvailabilityStatusUseCase : IRequestHandler<UpdateAvailabilityStatusCommand, bool>
+{
+    private readonly IRoomsRepository  _roomsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateAvailabilityStatusUseCase(IRoomsRepository roomsRepository, IUnitOfWork unitOfWork)
+    {
+        _roomsRepository = roomsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateAvailabilityStatusCommand command)
+    {
+        var room = await _roomsRepository.GetById(command.RoomId);
+        
+        if (room is null)
+            throw new NotFoundException("Room", command.RoomId);
+
+        var newAvailabilityStatus = Enum.Parse<RoomAvailabilityStatus>(command.AvailabilityStatus);
+        
+        room.UpdateAvailabilityStatus(newAvailabilityStatus);
+
+        try
+        {
+            await _roomsRepository.Update(room);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
