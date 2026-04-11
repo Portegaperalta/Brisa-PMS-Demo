f51d5c411a85858f48d7190d89a4aed7d8dbc5c7
Pablo <ortegaperaltap@gmail.com>
2026-04-10 22:39:36 -0400

    Create SetAsPendingRestock command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Rooms/Commands/SetAsPendingRestock/SetAsPendingRestockCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Rooms.Commands.SetAsPendingRestock;
+
+public class SetAsPendingRestockCommandValidator : AbstractValidator<SetAsPendingRestockCommand>
+{
+    public SetAsPendingRestockCommandValidator()
+    {
+        RuleFor(x => x.RoomId)
+            .NotEmpty().WithMessage("Field RoomId is required");
+    }
+}
