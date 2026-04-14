0768d2526e553bd05ce1e59405d720685dacc40f
Pablo <ortegaperaltap@gmail.com>
2026-04-13 20:25:36 -0400

    Refactor helper method CreateRoomType to initialize RoomBed VO inside constructor

--- BrisaPMS.UnitTests/Core/Domain/Rooms/RoomTests.cs
+++ BrisaPMS.UnitTests/Core/Domain/Rooms/RoomTests.cs
@@ -334,8 +334,7 @@ public class RoomTests
         return new RoomType(
             name,
             new RoomBaseRate(0.10m),
-            2,
-            BedType.Queen,
+            new RoomBed(BedType.Double, 1),
             new OccupancyPolicy(2, 1),
             "Spacious suite with ocean view");
     }
