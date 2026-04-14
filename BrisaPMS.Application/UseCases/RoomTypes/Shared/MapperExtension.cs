e95a137cb3b66e1ec5b1e572f4b228c2b05c0064
Pablo <ortegaperaltap@gmail.com>
2026-04-13 21:21:04 -0400

    Create RoomTypeDto and mapper extension

---
+++ BrisaPMS.Application/UseCases/RoomTypes/Shared/MapperExtension.cs
@@ -0,0 +1,21 @@
+﻿using BrisaPMS.Domain.RoomTypes;
+
+namespace BrisaPMS.Application.UseCases.RoomTypes.Shared;
+
+public static class MapperExtension
+{
+    public static RoomTypeDto ToDto(this RoomType roomType)
+    {
+        return new RoomTypeDto
+        {
+            Id = roomType.Id,
+            Name = roomType.Name,
+            Description = roomType.Description,
+            BaseRate = roomType.BaseRate.Rate,
+            NumberOfBeds = roomType.Beds.NumberOfBeds,
+            BedType = roomType.Beds.BedType.ToString(),
+            MaxOccupancyAdults = roomType.OccupancyPolicy.MaxOccupancyAdults,
+            MaxOccupancyChildren = roomType.OccupancyPolicy.MaxOccupancyChildren,
+        };
+    }
+}
