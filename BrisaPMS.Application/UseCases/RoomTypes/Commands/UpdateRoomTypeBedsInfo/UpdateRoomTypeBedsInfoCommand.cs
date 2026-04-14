31273de9140eb88b31bf2542a1f496e5045a07fe
Pablo <ortegaperaltap@gmail.com>
2026-04-13 21:13:29 -0400

    Create UpdateRoomTypeBedsInfo command, validator and use case

---
+++ BrisaPMS.Application/UseCases/RoomTypes/Commands/UpdateRoomTypeBedsInfo/UpdateRoomTypeBedsInfoCommand.cs
@@ -0,0 +1,10 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.RoomTypes.Commands.UpdateRoomTypeBedsInfo;
+
+public class UpdateRoomTypeBedsInfoCommand : IRequest<bool>
+{
+    public required Guid RoomTypeId { get; set; }
+    public required string BedType { get; set; }
+    public required int NumberOfBeds { get; set; }
+}
