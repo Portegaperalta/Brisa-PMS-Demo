dbbe1815f0405132d85f4b665f9512b7b0a403a9
Pablo <ortegaperaltap@gmail.com>
2026-04-13 21:28:35 -0400

    Create GetAllRoomTypes Query and UseCase

---
+++ BrisaPMS.Application/UseCases/RoomTypes/Queries/GetAllRoomTypes/GetAllRoomTypesUseCase.cs
@@ -0,0 +1,23 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.UseCases.RoomTypes.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.RoomTypes.Queries.GetAllRoomTypes;
+
+public class GetAllRoomTypesUseCase : IRequestHandler<GetAllRoomTypesQuery, List<RoomTypeDto>>
+{
+    private readonly IRoomTypesRepository _roomTypesRepository;
+
+    public GetAllRoomTypesUseCase(IRoomTypesRepository roomTypesRepository)
+    {
+        _roomTypesRepository = roomTypesRepository;
+    }
+
+    public async Task<List<RoomTypeDto>> Handle(GetAllRoomTypesQuery query)
+    {
+        var roomTypes = await _roomTypesRepository.GetAll();
+        
+        var roomTypesDtos = roomTypes.Select(r => r.ToDto()).ToList();
+        return roomTypesDtos;
+    }
+}
