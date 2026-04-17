cd203167125211b6bbbafd548514e4686b1f28f4
Pablo <ortegaperaltap@gmail.com>
2026-04-17 12:29:58 -0400

    Create GetAmenityById Query and use case

---
+++ BrisaPMS.Application/UseCases/Amenities/Queries/GetAmenityById/GetAmenityByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.UseCases.Amenities.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Amenities.Queries.GetAmenityById;
+
+public class GetAmenityByIdQuery : IRequest<AmenityDto>
+{
+    public required Guid AmenityId { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Amenities/Queries/GetAmenityById/GetAmenityByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.UseCases.Amenities.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Amenities.Queries.GetAmenityById;
+
+public class GetAmenityByIdUseCase : IRequestHandler<GetAmenityByIdQuery, AmenityDto>
+{
+    private readonly IAmenitiesRepository _amenitiesRepository;
+
+    public GetAmenityByIdUseCase(IAmenitiesRepository amenitiesRepository)
+    {
+        _amenitiesRepository = amenitiesRepository;
+    }
+
+    public async Task<AmenityDto> Handle(GetAmenityByIdQuery query)
+    {
+        var amenity = await _amenitiesRepository.GetById(query.AmenityId);
+
+        if (amenity is null)
+            throw new NotFoundException("Amenity", query.AmenityId);
+
+        var amenityDto = amenity.ToDto();
+        return amenityDto;
+    }
+}
