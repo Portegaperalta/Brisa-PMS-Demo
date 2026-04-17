41d7a897fca8f3abca8cddf4451e9e54ce372e2b
Pablo <ortegaperaltap@gmail.com>
2026-04-16 22:35:46 -0400

    Create GetGuestByHotelId query and use case

---
+++ BrisaPMS.Application/UseCases/Guests/Queries/GetGuestByHotelId/GetGuestByHotelIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.UseCases.Guests.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Guests.Queries.GetGuestByHotelId;
+
+public class GetGuestByHotelIdQuery : IRequest<GuestDto?>
+{
+    public required Guid HotelId { get; set; }
+}
