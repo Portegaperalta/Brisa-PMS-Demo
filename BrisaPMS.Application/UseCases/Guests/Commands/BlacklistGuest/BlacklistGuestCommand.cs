881b9743bf136efc8577eb250b5885eaaa9ffde5
Pablo <ortegaperaltap@gmail.com>
2026-04-16 21:31:38 -0400

    Create BlacklistGuest command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Guests/Commands/BlacklistGuest/BlacklistGuestCommand.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Guests.Commands.BlacklistGuest;
+
+public class BlacklistGuestCommand : IRequest<bool>
+{
+    public required Guid GuestId { get; set; }
+    public required string BlacklistedReason { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Guests/Commands/BlacklistGuest/BlacklistGuestCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Guests.Commands.BlacklistGuest;
+
+public class BlacklistGuestCommandValidator : AbstractValidator<BlacklistGuestCommand>
+{
+    public BlacklistGuestCommandValidator()
+    {
+        RuleFor(x => x.GuestId)
+            .NotEmpty().WithMessage("The field GuestId is required.");
+        
+        RuleFor(x => x.BlacklistedReason)
+            .NotEmpty().WithMessage("The field Blacklisted Reason is required.")
+            .MaximumLength(500).WithMessage("The field Blacklisted Reason can't exceed 500 characters.");
+    }
+}
