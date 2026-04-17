e34f56a1b5450c44dc804c6ae4b01fd0a9c50990
Pablo <ortegaperaltap@gmail.com>
2026-04-17 12:24:23 -0400

    Create GetAllAmenities Query and use case

---
+++ BrisaPMS.Application/UseCases/Amenities/Queries/GetAllAmenities/GetAllAmenitiesQuery.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.UseCases.Amenities.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Amenities.Queries.GetAllAmenities;
+
+public class GetAllAmenitiesQuery : IRequest<List<AmenityDto>>
+{
+}
---
+++ BrisaPMS.Application/UseCases/Amenities/Queries/GetAllAmenities/GetAllAmenitiesUseCase.cs
@@ -0,0 +1,22 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.UseCases.Amenities.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Amenities.Queries.GetAllAmenities;
+
+public class GetAllAmenitiesUseCase : IRequestHandler<GetAllAmenitiesQuery, List<AmenityDto>>
+{
+    private readonly IAmenitiesRepository _amenitiesRepository;
+
+    public GetAllAmenitiesUseCase(IAmenitiesRepository amenitiesRepository)
+    {
+        _amenitiesRepository = amenitiesRepository;
+    }
+
+    public async Task<List<AmenityDto>> Handle(GetAllAmenitiesQuery query)
+    {
+        var amenities = await _amenitiesRepository.GetAll();
+        var amenitiesDtos = amenities.Select(a => a.ToDto()).ToList();
+        return amenitiesDtos;
+    }
+}
