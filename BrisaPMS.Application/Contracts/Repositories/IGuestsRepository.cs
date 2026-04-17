5334d7b22dbf0aa49c79b2834f476b3d6fb0be3d
Pablo <ortegaperaltap@gmail.com>
2026-04-16 22:19:16 -0400

    Add GetAllByIdAsync method signature

--- BrisaPMS.Application/Contracts/Repositories/IGuestsRepository.cs
+++ BrisaPMS.Application/Contracts/Repositories/IGuestsRepository.cs
@@ -5,4 +5,5 @@ namespace BrisaPMS.Application.Contracts.Repositories;
 public interface IGuestsRepository : IRepository<Guest>
 {
     Task<Guest> GetByHotelIdAsync(Guid hotelId);
+    Task<List<Guest>> GetAllByHotelIdAsync(Guid hotelId);
 }
