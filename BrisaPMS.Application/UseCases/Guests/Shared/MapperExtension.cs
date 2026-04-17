c7616b212b6ab69be255cb5d5a9a0d1ac6400f48
Pablo <ortegaperaltap@gmail.com>
2026-04-16 22:25:35 -0400

    Create Guest Dto and MapperExtension

---
+++ BrisaPMS.Application/UseCases/Guests/Shared/MapperExtension.cs
@@ -0,0 +1,29 @@
+﻿using BrisaPMS.Domain.Guests;
+
+namespace BrisaPMS.Application.UseCases.Guests.Shared;
+
+public static class MapperExtension
+{
+    public static GuestDto ToDto(this Guest guest)
+    {
+        return new GuestDto
+        {
+            Id = guest.Id,
+            HotelId = guest.HotelId,
+            FirstName = guest.FirstName,
+            LastName = guest.LastName,
+            DocumentType = guest.DocumentType.ToString(),
+            DocumentNumber = guest.DocumentNumber,
+            Country = guest.Country,
+            Rnc = guest.Rnc.Value,
+            Email = guest.Email.Value,
+            PhoneNumber = guest.PhoneNumber.Value,
+            PreferredCurrency = guest.PreferredCurrency.ToString(),
+            PreferredLanguage = guest.PreferredLanguage,
+            IsVip = guest.IsVip,
+            IsBlackListed = guest.IsBlackListed,
+            BlackListedReason = guest.BlackListedReason,
+            Notes = guest.Notes,
+        };
+    }
+}
