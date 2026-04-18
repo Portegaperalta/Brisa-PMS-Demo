90b4d71380138636e6f39a927cb36a53c6c35ac8
Pablo <ortegaperaltap@gmail.com>
2026-04-18 13:11:53 -0400

    Create ChangeBookingSource command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/ChangeBookingSource/ChangeBookingSourceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using BrisaPMS.Domain.Bookings;
+using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.ChangeBookingSource;
+
+public class ChangeBookingSourceCommandValidator : AbstractValidator<ChangeBookingSourceCommand>
+{
+    public ChangeBookingSourceCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty().WithMessage("The field BookingId is required.");
+        
+        RuleFor(x => x.Source)
+            .NotEmpty().WithMessage("The field Booking Source is required.")
+            .MaximumLength(200).WithMessage("The field Booking Source can't exceed 200 characters.")
+            .Must(x => Enum.IsDefined(typeof(BookingSource), x)).WithMessage("Booking Source not supported");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/ChangeBookingSource/ChangeBookingSourceUseCase.cs
@@ -0,0 +1,44 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.Booking;
+using BrisaPMS.Domain.Bookings;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.ChangeBookingSource;
+
+public class ChangeBookingSourceUseCase : IRequestHandler<ChangeBookingSourceCommand, bool>
+{
+    private readonly IBookingsRepository _bookingsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ChangeBookingSourceUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingsRepository = bookingsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(ChangeBookingSourceCommand command)
+    {
+        var booking = await _bookingsRepository.GetById(command.BookingId);
+
+        if (booking is null)
+            throw new NotFoundException("Booking", command.BookingId);
+
+        var newBookingSource = Enum.Parse<BookingSource>(command.Source);
+        
+        booking.UpdateSource(newBookingSource);
+
+        try
+        {
+            await _bookingsRepository.Update(booking);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
