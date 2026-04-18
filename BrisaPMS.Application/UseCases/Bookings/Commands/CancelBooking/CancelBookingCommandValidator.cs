73c59acbf7d278049cc89e2f079038fdef4539e2
Pablo <ortegaperaltap@gmail.com>
2026-04-18 15:45:40 -0400

    Create CancelBooking command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/CancelBooking/CancelBookingCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.CancelBooking;
+
+public class CancelBookingCommandValidator : AbstractValidator<CancelBookingCommand>
+{
+    public CancelBookingCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty().WithMessage("The field BookingId is required");
+        
+        RuleFor(x => x.CancellationReason)
+            .NotEmpty().WithMessage("The field Cancellation Reason is required")
+            .MaximumLength(255).WithMessage("The field Cancellation Reason can't exceed 255 characters");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/CancelBooking/CancelBookingUseCase.cs
@@ -0,0 +1,41 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.Booking;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.CancelBooking;
+
+public class CancelBookingUseCase : IRequestHandler<CancelBookingCommand, bool>
+{
+    private readonly IBookingsRepository _bookingsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CancelBookingUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingsRepository = bookingsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(CancelBookingCommand command)
+    {
+        var booking = await _bookingsRepository.GetById(command.BookingId);
+
+        if (booking is null)
+            throw new NotFoundException("Booking", command.BookingId);
+
+        booking.Cancel(command.CancellationReason);
+
+        try
+        {
+            await _bookingsRepository.Update(booking);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
