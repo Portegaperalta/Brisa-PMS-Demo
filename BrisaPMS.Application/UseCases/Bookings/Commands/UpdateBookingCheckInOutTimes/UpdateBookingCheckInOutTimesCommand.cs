c18b575be33f84246f6015f1992109ba2d7140c4
Pablo <ortegaperaltap@gmail.com>
2026-04-18 15:20:18 -0400

    Create UpdateBookingCheckInOutTimes command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateBookingCheckInOutTimes/UpdateBookingCheckInOutTimesCommand.cs
@@ -0,0 +1,10 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateBookingCheckInOutTimes;
+
+public class UpdateBookingCheckInOutTimesCommand : IRequest<bool>
+{
+    public required Guid BookingId { get; set; }
+    public required DateTime CheckInTime { get; set; }
+    public required DateTime CheckOutTime { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateBookingCheckInOutTimes/UpdateBookingCheckInOutTimesCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateBookingCheckInOutTimes;
+
+public class UpdateBookingCheckInOutTimesCommandValidator : AbstractValidator<UpdateBookingCheckInOutTimesCommand>
+{
+    public UpdateBookingCheckInOutTimesCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty().WithMessage("The field BookingId is required.");
+        
+        RuleFor(x => x.CheckInTime)
+            .NotEmpty().WithMessage("The field Check-In Time is required.")
+            .LessThan(x => x.CheckOutTime)
+            .WithMessage("Check-In Time can't be later than Check-Out Time.");
+        
+        RuleFor(x => x.CheckOutTime)
+            .NotEmpty().WithMessage("The field Check-Out Time is required.")
+            .GreaterThan(x => x.CheckInTime)
+            .WithMessage("Check-Out Time can't be earlier than Check-In Time.");
+    }
+}
