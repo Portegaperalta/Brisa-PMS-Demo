b7866f436e4da330da067fe1254f317892b87c11
Pablo <ortegaperaltap@gmail.com>
2026-04-18 16:10:30 -0400

    Create UpdateTotalPrice command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateTotalPrice/UpdateTotalPriceCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateTotalPrice;
+
+public class UpdateTotalPriceCommandValidator : AbstractValidator<UpdateTotalPriceCommand>
+{
+    public UpdateTotalPriceCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty().WithMessage("The field BookingId is required.");
+        
+        RuleFor(x => x.TotalPrice)
+            .NotEmpty().WithMessage("The field Total Price is required.")
+            .GreaterThanOrEqualTo(0).WithMessage("Total price can't be negative.");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateTotalPrice/UpdateTotalPriceUseCase.cs
@@ -0,0 +1,42 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.Shared.ValueObjects;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateTotalPrice;
+
+public class UpdateTotalPriceUseCase : IRequestHandler<UpdateTotalPriceCommand, bool>
+{
+    private readonly IBookingsRepository _bookingsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateTotalPriceUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingsRepository = bookingsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateTotalPriceCommand command)
+    {
+        var booking = await _bookingsRepository.GetById(command.BookingId);
+
+        if (booking is null)
+            throw new NotFoundException("Booking", command.BookingId);
+
+        var newTotalPrice = new Money(command.TotalPrice);
+        booking.UpdateTotalPrice(newTotalPrice);
+
+        try
+        {
+            await _bookingsRepository.Update(booking);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
