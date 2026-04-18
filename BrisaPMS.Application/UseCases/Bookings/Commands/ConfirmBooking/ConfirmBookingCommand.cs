c122bde7ec8132bb3d32794cc1273916dd52827c
Pablo <ortegaperaltap@gmail.com>
2026-04-18 16:21:08 -0400

    Create ConfirmBooking command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/ConfirmBooking/ConfirmBookingCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.ConfirmBooking;
+
+public class ConfirmBookingCommand : IRequest<bool>
+{
+    public required Guid BookingId { get; init; }
+}
