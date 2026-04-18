d11eb886ba34dc00e4618cc3ba5dd5e8449ac4da
Pablo <ortegaperaltap@gmail.com>
2026-04-18 17:03:15 -0400

    Refactor CancelBookingUseCase to include room repository for updating room availability on booking cancellation

--- BrisaPMS.Application/UseCases/Bookings/Commands/CancelBooking/CancelBookingUseCase.cs
+++ BrisaPMS.Application/UseCases/Bookings/Commands/CancelBooking/CancelBookingUseCase.cs
@@ -3,17 +3,21 @@ using BrisaPMS.Application.Contracts.Repositories;
 using BrisaPMS.Application.Exceptions;
 using BrisaPMS.Application.Utilities.Mediator;
 using BrisaPMS.Domain.Booking;
+using BrisaPMS.Domain.Rooms;
 
 namespace BrisaPMS.Application.UseCases.Bookings.Commands.CancelBooking;
 
 public class CancelBookingUseCase : IRequestHandler<CancelBookingCommand, bool>
 {
     private readonly IBookingsRepository _bookingsRepository;
+    private readonly IRoomsRepository _roomsRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public CancelBookingUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    public CancelBookingUseCase(IBookingsRepository bookingsRepository, IRoomsRepository roomsRepository, 
+        IUnitOfWork unitOfWork)
     {
         _bookingsRepository = bookingsRepository;
+        _roomsRepository = roomsRepository;
         _unitOfWork = unitOfWork;
     }
 
@@ -23,12 +27,16 @@ public class CancelBookingUseCase : IRequestHandler<CancelBookingCommand, bool>
 
         if (booking is null)
             throw new NotFoundException("Booking", command.BookingId);
+        
+        var room = await _roomsRepository.GetById(booking.RoomId);
 
         booking.SetAsCancelled(command.CancellationReason);
+        room!.UpdateAvailabilityStatus(RoomAvailabilityStatus.Available);
 
         try
         {
             await _bookingsRepository.Update(booking);
+            await _roomsRepository.Update(room);
             await _unitOfWork.Persist();
             return true;
         }
