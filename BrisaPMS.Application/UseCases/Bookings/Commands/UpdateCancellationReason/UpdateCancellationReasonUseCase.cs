1d9ef3969b966c95cbf533c83a6a568b8aa2c795
Pablo <ortegaperaltap@gmail.com>
2026-04-18 16:38:41 -0400

    Create UpdateCancellationReason command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateCancellationReason/UpdateCancellationReasonUseCase.cs
@@ -0,0 +1,40 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateCancellationReason;
+
+public class UpdateCancellationReasonUseCase : IRequestHandler<UpdateCancellationReasonCommand, bool>
+{
+    private readonly IBookingsRepository _bookingsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateCancellationReasonUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingsRepository = bookingsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateCancellationReasonCommand command)
+    {
+        var booking = await _bookingsRepository.GetById(command.BookingId);
+        
+        if (booking is null)
+            throw new NotFoundException("Booking", command.BookingId);
+        
+        booking.UpdateCancellationReason(command.CancellationReason);
+        
+        try
+        {
+            await _bookingsRepository.Update(booking);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
