bca939fffd26e169cbc8b927e87ac3009b2af2dc
Pablo <ortegaperaltap@gmail.com>
2026-04-19 15:02:45 -0400

    Create Booking DTO and Mapper Extension

---
+++ BrisaPMS.Application/UseCases/Bookings/Queries/Shared/BookingDto.cs
@@ -0,0 +1,19 @@
+﻿namespace BrisaPMS.Application.UseCases.Bookings.Queries.Shared;
+
+public class BookingDto
+{
+    public required Guid Id { get; init; }
+    public required Guid HotelId { get; init; }
+    public required Guid RoomId { get; init; }
+    public required Guid GuestId { get; init; }
+    public required string Source { get; init; }
+    public required int NumberOfAdults { get; init; }
+    public required int NumberOfChildren { get; init; }
+    public required DateTime CheckInTime { get; init; }
+    public required DateTime CheckOutTime { get; init; }
+    public string? SpecialRequests { get; init; }
+    public required string Status { get; init; }
+    public string? CancellationReason { get; init; }
+    public required decimal TotalPrice { get; init; }
+    public Guid? DiscountId { get; init; }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Queries/Shared/MapperExtension.cs
@@ -0,0 +1,27 @@
+﻿using BrisaPMS.Domain.Bookings;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Queries.Shared;
+
+public static class MapperExtension
+{
+    public static BookingDto ToDto(this Booking booking)
+    {
+        return new BookingDto
+        {
+            Id = booking.Id,
+            HotelId = booking.HotelId,
+            RoomId = booking.RoomId,
+            GuestId = booking.GuestId,
+            Source = booking.Source.ToString(),
+            NumberOfAdults = booking.GuestCount.NumberOfAdults,
+            NumberOfChildren = booking.GuestCount.NumberOfChildren,
+            CheckInTime = booking.CheckInOutTimes.CheckInTime,
+            CheckOutTime = booking.CheckInOutTimes.CheckOutTime,
+            SpecialRequests = booking.SpecialRequests,
+            Status = booking.Status.ToString(),
+            CancellationReason = booking.CancellationReason,
+            TotalPrice = booking.TotalPrice.Amount,
+            DiscountId = booking.DiscountId,
+        };
+    }
+}
