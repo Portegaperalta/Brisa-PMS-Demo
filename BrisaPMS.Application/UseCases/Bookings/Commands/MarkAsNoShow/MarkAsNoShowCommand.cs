4de9961ccdf78bcea33d960421df4b674034efff
Pablo <ortegaperaltap@gmail.com>
2026-04-19 14:42:58 -0400

    Create MarkAsNoShow command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/MarkAsNoShow/MarkAsNoShowCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.MarkAsNoShow;
+
+public class MarkAsNoShowCommand : IRequest<bool>
+{
+    public required Guid BookingId { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/MarkAsNoShow/MarkAsNoShowCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.MarkAsNoShow;
+
+public class MarkAsNoShowCommandValidator : AbstractValidator<MarkAsNoShowCommand>
+{
+    public MarkAsNoShowCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty().WithMessage("The field BookingId is required.");
+    }
+}
