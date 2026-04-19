9f42ef6fbca25845fb67007b60ebb9c6e1e47b8f
Pablo <ortegaperaltap@gmail.com>
2026-04-19 13:36:44 -0400

    Create UpdateSpecialRequests command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateSpecialRequests/UpdateSpecialRequestsCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateSpecialRequests;
+
+public class UpdateSpecialRequestsCommandValidator : AbstractValidator<UpdateSpecialRequestsCommand>
+{
+    public UpdateSpecialRequestsCommandValidator()
+    {
+        RuleFor(x => x.BookingId)
+            .NotEmpty().WithMessage("The field BookingId is required.");
+        
+        RuleFor(x => x.SpecialRequests)
+            .NotEmpty().WithMessage("The field Special Requests is required.")
+            .MaximumLength(500).WithMessage("The field Special Requests can't exceed 500 characters.");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/UpdateSpecialRequests/UpdateSpecialRequestsUseCase.cs
@@ -0,0 +1,40 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.UpdateSpecialRequests;
+
+public class UpdateSpecialRequestsUseCase : IRequestHandler<UpdateSpecialRequestsCommand, bool>
+{
+    private readonly IBookingsRepository _bookingsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateSpecialRequestsUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingsRepository = bookingsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateSpecialRequestsCommand command)
+    {
+        var booking = await _bookingsRepository.GetById(command.BookingId);
+        
+        if (booking is null)
+            throw new NotFoundException("Booking", command.BookingId);
+        
+        booking.UpdateSpecialRequests(command.SpecialRequests);
+        
+        try
+        {
+            await _bookingsRepository.Update(booking);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
