097e870745af59c8430453b50f2b253f0aea014a
Pablo <ortegaperaltap@gmail.com>
2026-04-20 18:44:39 -0400

    Fix namespace to match folder structure

--- BrisaPMS.UnitTests/Core/Application/UseCases/Hotels/Queries/GetAllHotels/GetAllHotelsUseCaseTests.cs
+++ BrisaPMS.UnitTests/Core/Application/UseCases/Hotels/Queries/GetAllHotels/GetAllHotelsUseCaseTests.cs
@@ -7,7 +7,7 @@ using BrisaPMS.Domain.Shared.ValueObjects;
 using FluentAssertions;
 using NSubstitute;
 
-namespace BrisaPMS.UnitTests.Core.Application.UseCases.Hotels.Queries;
+namespace BrisaPMS.UnitTests.Core.Application.UseCases.Hotels.Queries.GetAllHotels;
 
 public class GetAllHotelsUseCaseTests
 {
