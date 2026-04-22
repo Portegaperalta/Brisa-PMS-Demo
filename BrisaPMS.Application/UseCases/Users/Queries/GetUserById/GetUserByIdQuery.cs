0a57e79f77fde1b8e266d5b908ae19f6e50619d4
Pablo <ortegaperaltap@gmail.com>
2026-04-22 14:52:41 -0400

    Create GetUserById query and use case

---
+++ BrisaPMS.Application/UseCases/Users/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.UseCases.Users.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Queries.GetUserById;
+
+public class GetUserByIdQuery : IRequest<UserDto>
+{
+    public required Guid UserId { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Users/Queries/GetUserById/GetUserByIdUseCase.cs
@@ -0,0 +1,23 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.UseCases.Users.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Queries.GetUserById;
+
+public class GetUserByIdUseCase : IRequestHandler<GetUserByIdQuery, UserDto>
+{
+    private readonly IUsersRepository _usersRepository;
+
+    public GetUserByIdUseCase(IUsersRepository usersRepository) { _usersRepository = usersRepository; }
+
+    public async Task<UserDto> Handle(GetUserByIdQuery query)
+    {
+        var user = await _usersRepository.GetById(query.UserId);
+        
+        if (user is null)
+            throw new NotFoundException("User", query.UserId);
+
+        return user.ToDto();
+    }
+}
