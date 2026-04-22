328da49a2c1c45376d8e4f5c5a4aae768283aa17
Pablo <ortegaperaltap@gmail.com>
2026-04-22 17:54:32 -0400

    Create UpdateHouseKeepingTaskPriority command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/UpdateHouseKeepingTaskPriority/UpdateHouseKeepingTaskPriorityCommand.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.UpdateHouseKeepingTaskPriority;
+
+public class UpdateHouseKeepingTaskPriorityCommand : IRequest<bool>
+{
+    public required Guid HouseKeepingTaskId { get; set; }
+    public required string TaskPriority { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/UpdateHouseKeepingTaskPriority/UpdateHouseKeepingTaskPriorityCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using BrisaPMS.Domain.HouseKeeping;
+using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.UpdateHouseKeepingTaskPriority;
+
+public class UpdateHouseKeepingTaskPriorityCommandValidator : AbstractValidator<UpdateHouseKeepingTaskPriorityCommand>
+{
+    public UpdateHouseKeepingTaskPriorityCommandValidator()
+    {
+        RuleFor(x => x.HouseKeepingTaskId)
+            .NotEmpty().WithMessage("The field HouseKeepingTaskId is required.");
+        
+        RuleFor(x => x.TaskPriority)
+            .NotEmpty().WithMessage("The field Task Priority is required.")
+            .Must(x => Enum.IsDefined(typeof(TaskPriority), x))
+            .WithMessage("Task priority not supported.");
+    }
+}
