a63bd037ae8cac42b06d6edc4986864312c67aab
Pablo <ortegaperaltap@gmail.com>
2026-04-22 17:38:10 -0400

    Create ReassignHouseKeepingTask command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/ReassignHouseKeepingTask/ReassignHouseKeepingTaskCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.ReassignHouseKeepingTask;
+
+public class ReassignHouseKeepingTaskCommandValidator : AbstractValidator<ReassignHouseKeepingTaskCommand>
+{
+    public ReassignHouseKeepingTaskCommandValidator()
+    {
+        RuleFor(x => x.HouseKeepingTaskId)
+            .NotEmpty().WithMessage("The field HouseKeepingTaskId is required.");
+        
+        RuleFor(x => x.AssignedTo)
+            .NotEmpty().WithMessage("The field Assigned To is required.");
+    }
+}
