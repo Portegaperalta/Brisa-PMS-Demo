75d1a16f71c58de2c06c3aed00ca6403ffb47183
Pablo <ortegaperaltap@gmail.com>
2026-04-22 17:46:43 -0400

    Create ChangeHouseKeepingTaskType command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/ChangeHouseKeepingTaskType/ChangeHouseKeepingTaskTypeUseCase.cs
@@ -0,0 +1,43 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.HouseKeeping;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.ChangeHouseKeepingTaskType;
+
+public class ChangeHouseKeepingTaskTypeUseCase : IRequestHandler<ChangeHouseKeepingTaskTypeCommand, bool>
+{
+    private readonly IHouseKeepingTasksRepository _houseKeepingTasksRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ChangeHouseKeepingTaskTypeUseCase(IHouseKeepingTasksRepository houseKeepingTasksRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _houseKeepingTasksRepository = houseKeepingTasksRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(ChangeHouseKeepingTaskTypeCommand command)
+    {
+        var houseKeepingTask = await _houseKeepingTasksRepository.GetById(command.HouseKeepingTaskId);
+        
+        if (houseKeepingTask is null)
+            throw new NotFoundException("HouseKeeping Task", command.HouseKeepingTaskId);
+
+        var newHouseKeepingTaskType = Enum.Parse<HouseKeepingTaskType>(command.HouseKeepingTaskType);
+        houseKeepingTask.ChangeTaskType(newHouseKeepingTaskType);
+        
+        try
+        {
+            await _houseKeepingTasksRepository.Update(houseKeepingTask);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
