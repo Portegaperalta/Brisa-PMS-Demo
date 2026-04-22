d1bc30a6d293842eff93975f6a84225c047e2bb4
Pablo <ortegaperaltap@gmail.com>
2026-04-21 22:08:29 -0400

    Create UpdateUserName command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Users/Commands/UpdateUserName/UpdateUserNameCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Users.Commands.UpdateUserName;
+
+public class UpdateUserNameCommandValidator : AbstractValidator<UpdateUserNameCommand>
+{
+    public UpdateUserNameCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("The field UserId is required.");
+        
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("The field First Name is required.")
+            .MaximumLength(250).WithMessage("The field First Name can't exceed 250 characters.");
+        
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("The field Last Name is required.")
+            .MaximumLength(250).WithMessage("The field Last Name can't exceed 250 characters.");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Users/Commands/UpdateUserName/UpdateUserNameUseCase.cs
@@ -0,0 +1,41 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Commands.UpdateUserName;
+
+public class UpdateUserNameUseCase : IRequestHandler<UpdateUserNameCommand, bool>
+{
+    private readonly IUsersRepository _usersRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateUserNameUseCase(IUsersRepository usersRepository, IUnitOfWork unitOfWork)
+    {
+        _usersRepository = usersRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateUserNameCommand command)
+    {
+        var user = await _usersRepository.GetById(command.UserId);
+        
+        if (user is null)
+            throw new NotFoundException("User", command.UserId);
+        
+        user.UpdateFirstName(command.FirstName);
+        user.UpdateLastName(command.LastName);
+        
+        try
+        {
+            await _usersRepository.Update(user);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
