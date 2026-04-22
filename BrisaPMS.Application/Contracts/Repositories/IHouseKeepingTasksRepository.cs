130dbb998e6177d50139ffa47974bb6460688f30
Pablo <ortegaperaltap@gmail.com>
2026-04-22 17:26:07 -0400

    Create interface for HouseKeeping tasks repository

---
+++ BrisaPMS.Application/Contracts/Repositories/IHouseKeepingTasksRepository.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Domain.HouseKeeping;
+
+namespace BrisaPMS.Application.Contracts.Repositories;
+
+public interface IHouseKeepingTasksRepository : IRepository<HouseKeepingTask>
+{
+    Task<IEnumerable<HouseKeepingTask>> GetByHotelIdAsync(Guid hotelId);
+}
