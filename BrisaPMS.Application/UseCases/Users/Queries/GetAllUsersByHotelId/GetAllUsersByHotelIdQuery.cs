675ab8b0821254214a1e6dd3c0e395e560c84600
Pablo <ortegaperaltap@gmail.com>
2026-04-22 14:53:01 -0400

    Create GetAllUsersByHotelId query and use case

---
+++ BrisaPMS.Application/UseCases/Users/Queries/GetAllUsersByHotelId/GetAllUsersByHotelIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.UseCases.Users.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Queries.GetAllUsersByHotelId;
+
+public class GetAllUsersByHotelIdQuery : IRequest<List<UserDto>>
+{
+    public required Guid HotelId { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/Users/Queries/GetAllUsersByHotelId/GetAllUsersByHotelIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.UseCases.Users.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Queries.GetAllUsersByHotelId;
+
+public class GetAllUsersByHotelIdUseCase : IRequestHandler<GetAllUsersByHotelIdQuery, List<UserDto>>
+{
+    private readonly IUsersRepository _usersRepository;
+    private readonly IHotelsRepository _hotelsRepository;
+
+    public GetAllUsersByHotelIdUseCase(IUsersRepository usersRepository, IHotelsRepository hotelsRepository)
+    {
+        _usersRepository = usersRepository;
+        _hotelsRepository = hotelsRepository;
+    }
+
+    public async Task<List<UserDto>> Handle(GetAllUsersByHotelIdQuery query)
+    {
+        var hotelExists = await _hotelsRepository.Exists(query.HotelId);
+
+        if (hotelExists is not true)
+            throw new NotFoundException("Hotel", query.HotelId);
+
+        var users = await _usersRepository.GetAllByHotelIdAsync(query.HotelId);
+        var usersDtos = users.Select(u => u.ToDto()).ToList();
+        return usersDtos;
+    }
+}
