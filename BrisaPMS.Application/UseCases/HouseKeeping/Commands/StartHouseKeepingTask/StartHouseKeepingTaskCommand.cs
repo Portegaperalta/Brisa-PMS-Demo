575b01e5c258fb7b91db8c09f6743c33fd3ac73b
Pablo <ortegaperaltap@gmail.com>
2026-04-22 18:17:42 -0400

    Create StartHouseKeepingTask command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/StartHouseKeepingTask/StartHouseKeepingTaskCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.StartHouseKeepingTask;
+
+public class StartHouseKeepingTaskCommand : IRequest<bool>
+{
+    public required Guid HouseKeepingTaskId { get; set; }
+}
