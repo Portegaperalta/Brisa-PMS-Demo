a9723e5a265e2ce74b5769e60dd0698b44fc1e98
Pablo <ortegaperaltap@gmail.com>
2026-04-21 20:01:04 -0400

    Create command, validator and use case for CreateUser

---
+++ BrisaPMS.Application/UseCases/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Commands.CreateUser;
+
+public class CreateUserCommand : IRequest<Guid>
+{
+    public required string Role { get; set; }
+    public Guid? HotelId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public string? PhoneNumber { get; set; }
+    public required string PreferredLanguage { get; set; }
+}
