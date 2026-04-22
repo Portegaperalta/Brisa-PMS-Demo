08cd7ec93a36b0c5fdb5496109b52b14707f16c4
Pablo <ortegaperaltap@gmail.com>
2026-04-22 18:39:40 -0400

    Create CancelHouseKeepingTask command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/CancelHouseKeepingTask/CancelHouseKeepingTaskCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.CancelHouseKeepingTask;
+
+public class CancelHouseKeepingTaskCommand : IRequest<bool>
+{
+    public required Guid HouseKeepingTaskId { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/CancelHouseKeepingTask/CancelHouseKeepingTaskUseCase.cs
@@ -0,0 +1,41 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+using BrisaPMS.Domain.HouseKeeping;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.CancelHouseKeepingTask;
+
+public class CancelHouseKeepingTaskUseCase : IRequestHandler<CancelHouseKeepingTaskCommand, bool>
+{
+    private readonly IHouseKeepingTasksRepository _houseKeepingTasksRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CancelHouseKeepingTaskUseCase(IHouseKeepingTasksRepository houseKeepingTasksRepository ,IUnitOfWork unitOfWork)
+    {
+        _houseKeepingTasksRepository = houseKeepingTasksRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(CancelHouseKeepingTaskCommand command)
+    {
+        var houseKeepingTask = await _houseKeepingTasksRepository.GetById(command.HouseKeepingTaskId);
+        
+        if (houseKeepingTask is null)
+            throw new NotFoundException("HouseKeeping Task", command.HouseKeepingTaskId);
+        
+        houseKeepingTask.UpdatedStatus(HouseKeepingTaskStatus.Cancelled);
+        
+        try
+        {
+            await _houseKeepingTasksRepository.Update(houseKeepingTask);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
