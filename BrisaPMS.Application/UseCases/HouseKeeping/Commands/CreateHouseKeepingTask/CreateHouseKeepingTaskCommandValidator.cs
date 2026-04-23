9fdc977fc63e7062c8767827d22051944db377dd
Pablo <ortegaperaltap@gmail.com>
2026-04-23 13:00:19 -0400

    Move NotEqual validation to ExpectedStartTime

--- BrisaPMS.Application/UseCases/HouseKeeping/Commands/CreateHouseKeepingTask/CreateHouseKeepingTaskCommandValidator.cs
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/CreateHouseKeepingTask/CreateHouseKeepingTaskCommandValidator.cs
@@ -29,14 +29,14 @@ public class CreateHouseKeepingTaskCommandValidator : AbstractValidator<CreateHo
         RuleFor(x => x.ExpectedStartTime)
             .NotEmpty().WithMessage("The field Expected Start Time is required.")
             .LessThan(x => x.ExpectedEndTime)
-            .WithMessage("Expected start time must be earlier than Expected end time.")
-            .NotEqual(x => x.ExpectedStartTime)
-            .WithMessage("Expected start time and end time cannot be the same.");
+            .WithMessage("Expected start time must be earlier than Expected end time.");
         
         RuleFor(x => x.ExpectedEndTime)
             .NotEmpty().WithMessage("The field Expected End Time is required.")
             .GreaterThan(x => x.ExpectedStartTime)
-            .WithMessage("Expected end time must be later than Expected start time.");
+            .WithMessage("Expected end time must be later than Expected start time.")
+            .NotEqual(x => x.ExpectedStartTime)
+            .WithMessage("Expected start time and end time cannot be the same.");
         
         RuleFor(x => x.Notes)
             .MaximumLength(500).WithMessage("The field Notes can't exceed 500 characters.");
