be60d828d0c304ebbc7ba59e77ed8a4d783593e6
Pablo <ortegaperaltap@gmail.com>
2026-04-23 11:04:49 -0400

    Rename GetByHotelId method signature for clarity and add new GetAllByRoomIdAsync

--- BrisaPMS.Application/Contracts/Repositories/IHouseKeepingTasksRepository.cs
+++ BrisaPMS.Application/Contracts/Repositories/IHouseKeepingTasksRepository.cs
@@ -4,5 +4,6 @@ namespace BrisaPMS.Application.Contracts.Repositories;
 
 public interface IHouseKeepingTasksRepository : IRepository<HouseKeepingTask>
 {
-    Task<IEnumerable<HouseKeepingTask>> GetByHotelIdAsync(Guid hotelId);
+    Task<IEnumerable<HouseKeepingTask>> GetAllByHotelIdAsync(Guid hotelId);
+    Task<IEnumerable<HouseKeepingTask>> GetAllByRoomIdAsync(Guid roomId);
 }
