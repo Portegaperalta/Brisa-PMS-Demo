4ec398a6d2639280a4f59ee78e94bd31b3e84907
Pablo <ortegaperaltap@gmail.com>
2026-04-23 11:21:09 -0400

    Create GetAllHouseKeepingTasks query and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Queries/GetAllHouseKeepingTasks/GetAllHouseKeepingTasksUseCase.cs
@@ -0,0 +1,22 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.UseCases.HouseKeeping.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Queries.GetAllHouseKeepingTasks;
+
+public class GetAllHouseKeepingTasksUseCase : IRequestHandler<GetAllHouseKeepingTasksQuery, List<HouseKeepingTaskDto>>
+{
+    private readonly IHouseKeepingTasksRepository _houseKeepingTasksRepository;
+
+    public GetAllHouseKeepingTasksUseCase(IHouseKeepingTasksRepository houseKeepingTasksRepository)
+    {
+        _houseKeepingTasksRepository = houseKeepingTasksRepository;
+    }
+
+    public async Task<List<HouseKeepingTaskDto>> Handle(GetAllHouseKeepingTasksQuery query)
+    {
+        var houseKeepingTasks = await _houseKeepingTasksRepository.GetAll();
+        var houseKeepingTasksDto =  houseKeepingTasks.Select(t => t.ToDto()).ToList();
+        return houseKeepingTasksDto;
+    }
+}
