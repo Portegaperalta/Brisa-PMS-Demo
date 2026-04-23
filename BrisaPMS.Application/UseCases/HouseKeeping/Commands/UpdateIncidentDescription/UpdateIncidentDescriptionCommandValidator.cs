0842b87859e20c0a4d8c9c16c693536663966dbe
Pablo <ortegaperaltap@gmail.com>
2026-04-22 23:56:42 -0400

    Create UpdateIncidentDescription command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/UpdateIncidentDescription/UpdateIncidentDescriptionCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.UpdateIncidentDescription;
+
+public class UpdateIncidentDescriptionCommandValidator : AbstractValidator<UpdateIncidentDescriptionCommand>
+{
+    public UpdateIncidentDescriptionCommandValidator()
+    {
+        RuleFor(x => x.HouseKeepingTaskId)
+            .NotEmpty().WithMessage("The field HouseKeepingTaskId is required.");
+        
+        RuleFor(x => x.IncidentDescription)
+            .NotEmpty().WithMessage("The field Incident Description is required.")
+            .MaximumLength(500).WithMessage("The field Incident Description can't exceed 500 characters.");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/UpdateIncidentDescription/UpdateIncidentDescriptionUseCase.cs
@@ -0,0 +1,41 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.UpdateIncidentDescription;
+
+public class UpdateIncidentDescriptionUseCase : IRequestHandler<UpdateIncidentDescriptionCommand, bool>
+{
+    private readonly IHouseKeepingTasksRepository _houseKeepingTasksRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateIncidentDescriptionUseCase(IHouseKeepingTasksRepository houseKeepingTasksRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _houseKeepingTasksRepository = houseKeepingTasksRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateIncidentDescriptionCommand command)
+    {
+        var houseKeepingTask = await _houseKeepingTasksRepository.GetById(command.HouseKeepingTaskId);
+        
+        if (houseKeepingTask is null)
+            throw new NotFoundException("HouseKeeping Task", command.HouseKeepingTaskId);
+        
+        houseKeepingTask.UpdateIncidentDescription(command.IncidentDescription);
+        
+        try
+        {
+            await _houseKeepingTasksRepository.Update(houseKeepingTask);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
