6616308890ae7b66859384abcbbb628f558c7aa6
Pablo <ortegaperaltap@gmail.com>
2026-04-23 11:00:50 -0400

    Create query and use case for GetAllHouseKeepingTasksByHotelId

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Queries/GetAllHouseKeepingTasksByHotelId/GetAllHouseKeepingTasksByHotelIdUseCase.cs
@@ -0,0 +1,33 @@
+﻿using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.UseCases.HouseKeeping.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Queries.GetAllHouseKeepingTasksByHotelId;
+
+public class GetAllHouseKeepingTasksByHotelIdUseCase : 
+    IRequestHandler<GetAllHouseKeepingTasksByHotelIdQuery, List<HouseKeepingTaskDto>>
+{
+    private readonly IHouseKeepingTasksRepository _houseKeepingTasksRepository;
+    private readonly IHotelsRepository _hotelsRepository;
+
+    public GetAllHouseKeepingTasksByHotelIdUseCase(IHouseKeepingTasksRepository houseKeepingTasksRepository,
+        IHotelsRepository hotelsRepository)
+    {
+        _houseKeepingTasksRepository = houseKeepingTasksRepository;
+        _hotelsRepository = hotelsRepository;
+    }
+
+    public async Task<List<HouseKeepingTaskDto>> Handle(GetAllHouseKeepingTasksByHotelIdQuery query)
+    {
+        var hotelExists = await _hotelsRepository.Exists(query.HotelId);
+
+        if (hotelExists is not true)
+            throw new NotFoundException("Hotel", query.HotelId);
+
+        var houseKeepingTasks = await _houseKeepingTasksRepository.GetByHotelIdAsync(query.HotelId);
+        var houseKeepingTasksDtos = houseKeepingTasks.Select(t => t.ToDto()).ToList();
+        
+        return houseKeepingTasksDtos;
+    }
+}
