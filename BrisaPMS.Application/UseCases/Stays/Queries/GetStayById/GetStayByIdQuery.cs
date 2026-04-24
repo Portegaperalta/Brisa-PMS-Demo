0dc4873c127e23ce6e38cebb24a11e5516691001
Pablo <ortegaperaltap@gmail.com>
2026-04-24 11:32:02 -0400

    Create GetStayById quey and use case

---
+++ BrisaPMS.Application/UseCases/Stays/Queries/GetStayById/GetStayByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BrisaPMS.Application.UseCases.Stays.Shared;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Stays.Queries.GetStayById;
+
+public class GetStayByIdQuery : IRequest<StayDto>
+{
+    public required Guid StayId { get; set; }
+}
