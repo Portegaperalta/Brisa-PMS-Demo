9cdf1775a38598e887e52e32d541483c701505b8
Pablo <ortegaperaltap@gmail.com>
2026-04-24 10:17:09 -0400

    Create IncreaseNightCount command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Stays/Commands/IncreaseNightCount/IncreaseNightCountCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BrisaPMS.Application.UseCases.Stays.Commands.IncreaseNightCount;
+
+public class IncreaseNightCountCommandValidator : AbstractValidator<IncreaseNightCountCommand>
+{
+    public IncreaseNightCountCommandValidator()
+    {
+        RuleFor(x => x.StayId)
+            .NotEmpty().WithMessage("The field StayId is required.");
+    }
+}
---
+++ BrisaPMS.Application/UseCases/Stays/Commands/IncreaseNightCount/IncreaseNightCountUseCase.cs
@@ -0,0 +1,40 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Stays.Commands.IncreaseNightCount;
+
+public class IncreaseNightCountUseCase : IRequestHandler<IncreaseNightCountCommand, bool>
+{
+    private readonly IStaysRepository _staysRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public IncreaseNightCountUseCase(IStaysRepository staysRepository, IUnitOfWork unitOfWork)
+    {
+        _staysRepository = staysRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(IncreaseNightCountCommand command)
+    {
+        var stay = await _staysRepository.GetById(command.StayId);
+
+        if (stay is null)
+            throw new NotFoundException("Stay", command.StayId);
+        
+        stay.IncreaseNightCount();
+
+        try
+        {
+            await _staysRepository.Update(stay);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
