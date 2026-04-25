c848d31e149988ec73b59d62788be462ded07af6
Pablo <ortegaperaltap@gmail.com>
2026-04-24 22:28:54 -0400

    Create Db context and add project reference to Domain and Application layers

---
+++ BrisaPMS.Persistence/BrisaPmsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrisaPMS.Persistence
+{
+    public class BrisaPmsDbContext : DbContext
+    {
+        public BrisaPmsDbContext(DbContextOptions<BrisaPmsDbContext> options) : base(options)
+        {
+        }
+
+        protected BrisaPmsDbContext()
+        {
+        }
+    }
+}
