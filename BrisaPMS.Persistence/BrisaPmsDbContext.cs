e02f4c19e75467affd738d6b0a1b44669b493ba0
Pablo <ortegaperaltap@gmail.com>
2026-04-24 22:42:18 -0400

    Add DbSets for entities that already have commands and queries

--- BrisaPMS.Persistence/BrisaPmsDbContext.cs
+++ BrisaPMS.Persistence/BrisaPmsDbContext.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BrisaPMS.Domain.Amenities;
+using BrisaPMS.Domain.Bookings;
+using BrisaPMS.Domain.Companies;
+using BrisaPMS.Domain.Guests;
+using BrisaPMS.Domain.Hotels;
+using BrisaPMS.Domain.HouseKeeping;
+using BrisaPMS.Domain.Rooms;
+using BrisaPMS.Domain.RoomTypes;
+using BrisaPMS.Domain.Stays;
+using BrisaPMS.Domain.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace BrisaPMS.Persistence
 {
@@ -11,5 +21,16 @@ namespace BrisaPMS.Persistence
         protected BrisaPmsDbContext()
         {
         }
+
+        public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HouseKeepingTask> HouseKeepingTasks { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Stay> Stays { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
