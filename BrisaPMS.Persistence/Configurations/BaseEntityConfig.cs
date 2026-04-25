b2de869601657f43ea72ae831fb005d948af82d9
Pablo <ortegaperaltap@gmail.com>
2026-04-25 13:25:26 -0400

    Create config for BaseEntity

---
+++ BrisaPMS.Persistence/Configurations/BaseEntityConfig.cs
@@ -0,0 +1,24 @@
+﻿using BrisaPMS.Domain.Shared.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BrisaPMS.Persistence.Configurations;
+
+public abstract class BaseEntityConfig<T> : IEntityTypeConfiguration<T>
+    where T : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.Property(p => p.CreatedBy)
+               .IsRequired();
+
+        builder.Property(p => p.CreatedAt)
+               .IsRequired();
+
+        builder.Property(p => p.UpdatedBy)
+               .HasDefaultValue(null);
+
+        builder.Property(p => p.UpdatedAt)
+            .HasDefaultValue(null);
+    }
+}
