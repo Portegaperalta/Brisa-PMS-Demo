ad4d0186f31e5c14b1a5c31d1d0e6c9644d6e755
Pablo <ortegaperaltap@gmail.com>
2026-04-26 12:12:23 -0400

    Create Stay config for EF Core

---
+++ BrisaPMS.Persistence/Configurations/StayConfig.cs
@@ -0,0 +1,43 @@
+﻿using BrisaPMS.Domain.Bookings;
+using BrisaPMS.Domain.Guests;
+using BrisaPMS.Domain.Stays;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BrisaPMS.Persistence.Configurations;
+
+public class StayConfig : BaseEntityConfig<Stay>
+{
+    public override void Configure(EntityTypeBuilder<Stay> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(s => s.Id)
+               .IsRequired();
+
+        builder.HasOne<Guest>()
+               .WithMany()
+               .HasForeignKey(s => s.GuestId)
+               .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.HasOne<Booking>()
+               .WithMany()
+               .HasForeignKey(s => s.BookingId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.OwnsOne(s => s.TimeInterval, timeInterval =>
+        {
+            timeInterval.Property(t => t.ActualCheckIn)
+                        .IsRequired();
+
+            timeInterval.Property(t => t.ActualCheckOut);
+        });
+
+        builder.Property(s => s.NightCount)
+               .IsRequired();
+
+        builder.Property(s => s.Status)
+               .IsRequired()
+               .HasConversion<string>();
+    }
+}
