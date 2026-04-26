4549df59edf941cbec07785553453bc43b5e8bc2
Pablo <ortegaperaltap@gmail.com>
2026-04-26 18:59:08 -0400

    Create and register concrete UnitOfWork

---
+++ BrisaPMS.Persistence/UnitsOfWork/UnitOfWorkEfCore.cs
@@ -0,0 +1,23 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+
+namespace BrisaPMS.Persistence.UnitsOfWork;
+
+public class UnitOfWorkEfCore : IUnitOfWork
+{
+    private readonly BrisaPmsDbContext _context;
+    
+    public UnitOfWorkEfCore(BrisaPmsDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task Persist()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public Task Revert()
+    {
+        return Task.CompletedTask;
+    }
+}
