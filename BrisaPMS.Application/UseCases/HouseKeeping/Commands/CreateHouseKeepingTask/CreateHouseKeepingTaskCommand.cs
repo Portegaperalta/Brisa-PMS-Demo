7df37ca266221ee1979cdbdf2c1593b7150400b0
Pablo <ortegaperaltap@gmail.com>
2026-04-26 16:02:54 -0400

    Add requird HotelId field

--- BrisaPMS.Application/UseCases/HouseKeeping/Commands/CreateHouseKeepingTask/CreateHouseKeepingTaskCommand.cs
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/CreateHouseKeepingTask/CreateHouseKeepingTaskCommand.cs
@@ -4,6 +4,7 @@ namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.CreateHouseKeeping
 
 public class CreateHouseKeepingTaskCommand : IRequest<Guid>
 {
+    public Guid HotelId { get; set; }
     public required Guid RoomId { get; set; }
     public required Guid AssignedTo { get; set; }
     public required Guid AssignedBy { get; set; }
--- BrisaPMS.Application/UseCases/HouseKeeping/Commands/CreateHouseKeepingTask/CreateHouseKeepingTaskCommandValidator.cs
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/CreateHouseKeepingTask/CreateHouseKeepingTaskCommandValidator.cs
@@ -7,6 +7,9 @@ public class CreateHouseKeepingTaskCommandValidator : AbstractValidator<CreateHo
 {
     public CreateHouseKeepingTaskCommandValidator()
     {
+        RuleFor(x => x.HotelId)
+            .NotEmpty().WithMessage("The field HotelId is required.");
+        
         RuleFor(x => x.RoomId)
             .NotEmpty().WithMessage("The field RoomId is required.");
         
