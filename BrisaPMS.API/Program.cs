6c34bf843c969c70fb606eda36799f0b8171d8d3
Pablo <ortegaperaltap@gmail.com>
2026-04-26 21:34:42 -0400

    Remove API Project

--- BrisaPMS.API/Program.cs
+++ BrisaPMS.API/Program.cs
@@ -7,10 +7,8 @@ namespace BrisaPMS.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
