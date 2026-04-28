7b6953a8b26db256a31f46001c642c6e8236c6b4
Pablo <ortegaperaltap@gmail.com>
2026-04-28 19:52:03 -0400

    Add custom error message to required fields

--- BrisaPMS.API/DTOs/Users/CreateUserDTO.cs
+++ BrisaPMS.API/DTOs/Users/CreateUserDTO.cs
@@ -4,26 +4,26 @@ namespace BrisaPMS.API.DTOs.Users
 {
     public class CreateUserDTO
     {
-        [Required]
+        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(25)]
         public required string Role { get; set; }
 
         public Guid? HotelId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(250)]
         public required string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(250)]
         public required string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(254)]
         [EmailAddress]
         public required string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(512, MinimumLength = 8)]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$",
         ErrorMessage = "Password must contain at least one uppercase letter, one number, and one special character.")]
@@ -33,7 +33,7 @@ namespace BrisaPMS.API.DTOs.Users
         [RegularExpression(@"^\+?[1-9]\d{1,14}$", ErrorMessage = "Must be a valid phone number.")]
         public string? PhoneNumber { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(5)]
         public required string PreferredLanguage { get; set; }
     }
