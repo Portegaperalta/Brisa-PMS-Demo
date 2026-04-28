d8fafd118309a7b28b5ca37fad93a952f09f8c3b
Pablo <ortegaperaltap@gmail.com>
2026-04-28 12:19:32 -0400

    Add UpdateAmenityDetails DTO for API layer

---
+++ BrisaPMS.API/DTOs/Amenities/UpdateAmenityDetailsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrisaPMS.API.DTOs.Amenities
+{
+    public class UpdateAmenityDetailsDTO
+    {
+        [Required]
+        public required Guid AmenityId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public required string Description { get; set; }
+    }
+}
