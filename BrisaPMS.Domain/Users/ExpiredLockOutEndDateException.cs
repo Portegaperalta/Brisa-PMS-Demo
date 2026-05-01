2c4131125efc6d0dc65a6621be15a7585c28f2b7
Pablo <ortegaperaltap@gmail.com>
2026-05-01 12:53:58 -0400

    delete ExpiredLockOutEndDateException

--- BrisaPMS.Domain/Users/ExpiredLockOutEndDateException.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BrisaPMS.Domain.Users;
-
-public class ExpiredLockOutEndDateException : Exception
-{
-    public ExpiredLockOutEndDateException() : base("Lockout expiry date cannot be older than current date") { }
-}
