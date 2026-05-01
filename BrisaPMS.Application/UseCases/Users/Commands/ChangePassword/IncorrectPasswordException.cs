0a7351d503b0a77b654104b8a01f97b65d2eb24f
Pablo <ortegaperaltap@gmail.com>
2026-05-01 17:30:25 -0400

    Create custom IncorrectPasswordException

---
+++ BrisaPMS.Application/UseCases/Users/Commands/ChangePassword/IncorrectPasswordException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrisaPMS.Application.UseCases.Users.Commands.ChangePassword
+{
+    public class IncorrectPasswordException : Exception
+    {
+        public IncorrectPasswordException() : base("Current password is incorrect") { }
+    }
+}
