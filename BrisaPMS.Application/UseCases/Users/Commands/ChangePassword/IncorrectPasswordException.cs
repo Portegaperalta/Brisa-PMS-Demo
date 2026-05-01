89be08c6a362fa5390065e5868508815bdb988d0
Pablo <ortegaperaltap@gmail.com>
2026-05-01 17:32:00 -0400

    remove unecessary usings

--- BrisaPMS.Application/UseCases/Users/Commands/ChangePassword/IncorrectPasswordException.cs
+++ BrisaPMS.Application/UseCases/Users/Commands/ChangePassword/IncorrectPasswordException.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BrisaPMS.Application.UseCases.Users.Commands.ChangePassword
+﻿namespace BrisaPMS.Application.UseCases.Users.Commands.ChangePassword
 {
     public class IncorrectPasswordException : Exception
     {
