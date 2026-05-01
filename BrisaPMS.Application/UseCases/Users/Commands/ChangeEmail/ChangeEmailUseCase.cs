a32ed4d2bcfb81ebb1fa27e597d9e477b8745bb4
Pablo <ortegaperaltap@gmail.com>
2026-05-01 17:45:20 -0400

    Refactor use case to update both entity and appUser emails

--- BrisaPMS.Application/UseCases/Users/Commands/ChangeEmail/ChangeEmailUseCase.cs
+++ BrisaPMS.Application/UseCases/Users/Commands/ChangeEmail/ChangeEmailUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using BrisaPMS.Application.Contracts.Persistence;
 using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Contracts.Services;
 using BrisaPMS.Application.Exceptions;
 using BrisaPMS.Application.Utilities.Mediator;
 using BrisaPMS.Domain.Shared.ValueObjects;
@@ -9,11 +10,14 @@ namespace BrisaPMS.Application.UseCases.Users.Commands.ChangeEmail;
 public class ChangeEmailUseCase : IRequestHandler<ChangeEmailCommand, bool>
 {
     private readonly IUsersRepository _usersRepository;
+    private readonly IIdentityService _identityService;
     private readonly IUnitOfWork _unitOfWork;
 
-    public ChangeEmailUseCase(IUsersRepository usersRepository, IUnitOfWork unitOfWork)
+    public ChangeEmailUseCase(IUsersRepository usersRepository, IUnitOfWork unitOfWork, 
+        IIdentityService identityService)
     {
         _usersRepository = usersRepository;
+        _identityService = identityService;
         _unitOfWork = unitOfWork;
     }
 
@@ -23,14 +27,19 @@ public class ChangeEmailUseCase : IRequestHandler<ChangeEmailCommand, bool>
         
         if (user is null)
             throw new NotFoundException("User", command.UserId);
-        
+
+        var isEmailUnique = await _identityService.IsEmailUniqueAsync(command.Email);
+
+        if (isEmailUnique is not true)
+            throw new InvalidOperationException("Email already in use");
+
         var newEmail = new Email(command.Email);
-        
         user.ChangeEmail(newEmail);
         
         try
         {
             await _usersRepository.Update(user);
+            await _identityService.UpdateEmailAsync(command.UserId, command.Email);
             await _unitOfWork.Persist();
             return true;
         }
