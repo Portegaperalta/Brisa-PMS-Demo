e146e1a325c6646efb2673fd3af55f81f066999e
Pablo <ortegaperaltap@gmail.com>
2026-05-04 16:25:41 -0400

    Create DeleteStay command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Stays/Commands/DeleteStay/DeleteStayUseCase.cs
@@ -0,0 +1,36 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Stays.Commands.DeleteStay;
+
+public class DeleteStayUseCase : IRequestHandler<DeleteStayCommand, bool>
+{
+    private readonly IStaysRepository _staysRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteStayUseCase(IStaysRepository staysRepository, IUnitOfWork unitOfWork)
+    {
+        _staysRepository = staysRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(DeleteStayCommand command)
+    {
+        var stay = await _staysRepository.GetById(command.Id) ??
+                   throw new NotFoundException("Stay", command.Id);
+
+        try
+        {
+            await _staysRepository.Delete(stay);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
