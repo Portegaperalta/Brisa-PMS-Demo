43d42e5a8185f19e84dc941b20d282f0c5c089ff
Pablo <ortegaperaltap@gmail.com>
2026-05-03 22:21:50 -0400

    Add Login endpoint

--- BrisaPMS.API/Controllers/UsersController.cs
+++ BrisaPMS.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using BrisaPMS.Application.UseCases.Users.Commands.CreateUser;
+﻿using BrisaPMS.API.DTOs.Auth;
+using BrisaPMS.Application.UseCases.Users.Commands.CreateUser;
+using BrisaPMS.Application.UseCases.Users.Commands.Login;
 using BrisaPMS.Application.Utilities.Mediator;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,14 @@ namespace BrisaPMS.API.Controllers
         public UsersController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResponseDTO>> Login([FromBody] LoginCommand command)
+        {
+            var result = await _mediator.Send(command);
+            var authResponseDto = new AuthenticationResponseDTO { Token = result };
+            return Ok(authResponseDto);
         }
 
         [HttpPost("register")]
