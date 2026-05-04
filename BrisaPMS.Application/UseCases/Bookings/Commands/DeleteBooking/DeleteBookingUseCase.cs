71404ac29129546892e40b8aef83100240df7e04
Pablo <ortegaperaltap@gmail.com>
2026-05-04 15:59:52 -0400

    Create DeleteBooking command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Bookings/Commands/DeleteBooking/DeleteBookingUseCase.cs
@@ -0,0 +1,36 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Bookings.Commands.DeleteBooking;
+
+public class DeleteBookingUseCase : IRequestHandler<DeleteBookingCommand, bool>
+{
+    private readonly IBookingsRepository _bookingsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteBookingUseCase(IBookingsRepository bookingsRepository, IUnitOfWork unitOfWork)
+    {
+        _bookingsRepository = bookingsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(DeleteBookingCommand command)
+    {
+        var booking = await _bookingsRepository.GetById(command.Id) ??
+                      throw new NotFoundException("Booking", command.Id);
+        
+        try
+        {
+            await _bookingsRepository.Delete(booking);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
