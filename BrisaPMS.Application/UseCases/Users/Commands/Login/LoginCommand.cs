af35eb63dd160e36e772c5dcc5c190a39eb87e1b
Pablo <ortegaperaltap@gmail.com>
2026-05-03 21:57:01 -0400

    Create Login Command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Users/Commands/Login/LoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Users.Commands.Login
+{
+    public class LoginCommand : IRequest<string>
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
