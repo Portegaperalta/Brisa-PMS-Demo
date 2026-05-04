116c3b7afb85d79b4f2aa5f5b22219b8698bbb9d
Pablo <ortegaperaltap@gmail.com>
2026-05-04 15:49:09 -0400

    Create DeleteAmenity command, validator and use case

---
+++ BrisaPMS.Application/UseCases/Amenities/Commands/DeleteAmenity/DeleteAmenityCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.Amenities.Commands.DeleteAmenity;
+
+public class DeleteAmenityCommand : IRequest<bool>
+{
+    public required Guid Id { get; set; }
+}
