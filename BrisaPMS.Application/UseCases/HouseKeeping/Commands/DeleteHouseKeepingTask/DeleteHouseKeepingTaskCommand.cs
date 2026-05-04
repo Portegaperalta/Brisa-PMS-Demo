cb6e8fef1cfc2322e666185772afafa88c22cd9d
Pablo <ortegaperaltap@gmail.com>
2026-05-04 16:13:18 -0400

    Create DeleteHouseKeepingTask command, validator and use case

---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/DeleteHouseKeepingTask/DeleteHouseKeepingTaskCommand.cs
@@ -0,0 +1,8 @@
+﻿using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.DeleteHouseKeepingTask;
+
+public class DeleteHouseKeepingTaskCommand : IRequest<bool>
+{
+    public required Guid Id { get; set; }
+}
---
+++ BrisaPMS.Application/UseCases/HouseKeeping/Commands/DeleteHouseKeepingTask/DeleteHouseKeepingTaskUseCase.cs
@@ -0,0 +1,37 @@
+﻿using BrisaPMS.Application.Contracts.Persistence;
+using BrisaPMS.Application.Contracts.Repositories;
+using BrisaPMS.Application.Exceptions;
+using BrisaPMS.Application.Utilities.Mediator;
+
+namespace BrisaPMS.Application.UseCases.HouseKeeping.Commands.DeleteHouseKeepingTask;
+
+public class DeleteHouseKeepingTaskUseCase : IRequestHandler<DeleteHouseKeepingTaskCommand, bool>
+{
+    private readonly IHouseKeepingTasksRepository _houseKeepingTasksRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteHouseKeepingTaskUseCase(IHouseKeepingTasksRepository houseKeepingTasksRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _houseKeepingTasksRepository = houseKeepingTasksRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(DeleteHouseKeepingTaskCommand command)
+    {
+        var houseKeepingTask = await _houseKeepingTasksRepository.GetById(command.Id) ??
+                               throw new NotFoundException("HouseKeeping Task", command.Id);
+
+        try
+        {
+            await _houseKeepingTasksRepository.Delete(houseKeepingTask);
+            await _unitOfWork.Persist();
+            return true;
+        }
+        catch (Exception)
+        {
+            await _unitOfWork.Revert();
+            throw;
+        }
+    }
+}
